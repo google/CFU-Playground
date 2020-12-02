f7e5ac1983f4dd2e150f330a2f4700ec37501a7e
Tim Callahan <tcal@google.com>
2020-12-02 01:03:34 -0800

    Fix TFLM ErrorReporter: use stdout instead of stderr.
    Signed-off-by: Tim Callahan <tcal@google.com>

--- proj/proj_template/third_party/tflm_gen/tensorflow/lite/micro/debug_log.cc
+++ proj/proj_template/third_party/tflm_gen/tensorflow/lite/micro/debug_log.cc
@@ -45,6 +45,7 @@ extern "C" void DebugLog(const char* s) {
   // Reusing TF_LITE_STRIP_ERROR_STRINGS to disable DebugLog completely to get
   // maximum reduction in binary size. This is because we have DebugLog calls
   // via TF_LITE_CHECK that are not stubbed out by TF_LITE_REPORT_ERROR.
-  fprintf(stderr, "%s", s);
+  //fprintf(stderr, "%s", s);
+  puts(s);
 #endif
 }
