a58fcb1b05a44cf10c5221fcda635aa7d734f461
Rachel Sugrono <rsugrono@google.com>
2021-01-14 14:42:43 +1100

    proj_accel_1: The new directory with all our files
    This new directory contains all the files we need for our double
    comparator CFU (cfu.py, cfu_gen.py and conv.h).

---
+++ proj/proj_accel_1/basic_cfu/src/cfu_emulated.c
@@ -0,0 +1,1 @@
+../../cfu_emulated.c
---
+++ proj/proj_accel_1/cfu.h
@@ -0,0 +1,48 @@
+
+#include "riscv.h"
+
+/* riscv.h defines a macro:
+
+    #define opcode_R(opcode, funct3, funct7, rs1, rs2)
+
+   that returns at 32b value.  The opcode must be "CUSTOM0" (also defined in riscv.h).
+
+   'func3' is used as functionID sent to the CFU.
+
+*/
+
+// =============== Access the custom instruction
+
+// generic name for each custom instruction
+#define cfu_op0_hw(rs1, rs2)  opcode_R(CUSTOM0, 0, 0, (rs1), (rs2))
+#define cfu_op1_hw(rs1, rs2)  opcode_R(CUSTOM0, 1, 0, (rs1), (rs2))
+#define cfu_op2_hw(rs1, rs2)  opcode_R(CUSTOM0, 2, 0, (rs1), (rs2))
+
+
+
+
+// =============== Software (C implementation of custom instructions)
+
+uint32_t Cfu(uint32_t functionid, uint32_t rs1, uint32_t rs2);
+
+// generic name for each custom instruction
+#define cfu_op0_sw(rs1, rs2)  Cfu(0, rs1, rs2)
+#define cfu_op1_sw(rs1, rs2)  Cfu(1, rs1, rs2)
+#define cfu_op2_sw(rs1, rs2)  Cfu(2, rs1, rs2)
+
+
+
+// =============== Switch HW vs SW
+
+#ifdef CFU_FORCE_SW
+#define cfu_op0(rs1, rs2)       cfu_op0_sw(rs1, rs2)
+#define cfu_op1(rs1, rs2)       cfu_op1_sw(rs1, rs2)
+#define cfu_op2(rs1, rs2)       cfu_op2_sw(rs1, rs2)
+
+#else
+
+#define cfu_op0(rs1, rs2)       cfu_op0_hw((rs1), (rs2))
+#define cfu_op1(rs1, rs2)       cfu_op1_hw((rs1), (rs2))
+#define cfu_op2(rs1, rs2)       cfu_op2_hw((rs1), (rs2))
+
+#endif
