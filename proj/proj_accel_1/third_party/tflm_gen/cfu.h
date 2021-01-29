5f8d98c5dcaec49ffa5afacfdbdee033963ad55f
Rachel Sugrono <rsugrono@google.com>
2021-01-29 11:42:21 +1100

    define all the CFUs
    Define all 8 of the CFUs available for future use.
    
    Signed-off-by: Rachel Sugrono <rsugrono@google.com>

--- proj/proj_accel_1/third_party/tflm_gen/cfu.h
+++ proj/proj_accel_1/third_party/tflm_gen/cfu.h
@@ -17,7 +17,11 @@
 #define cfu_op0_hw(rs1, rs2)  opcode_R(CUSTOM0, 0, 0, (rs1), (rs2))
 #define cfu_op1_hw(rs1, rs2)  opcode_R(CUSTOM0, 1, 0, (rs1), (rs2))
 #define cfu_op2_hw(rs1, rs2)  opcode_R(CUSTOM0, 2, 0, (rs1), (rs2))
-
+#define cfu_op3_hw(rs1, rs2)  opcode_R(CUSTOM0, 3, 0, (rs1), (rs2))
+#define cfu_op4_hw(rs1, rs2)  opcode_R(CUSTOM0, 4, 0, (rs1), (rs2))
+#define cfu_op5_hw(rs1, rs2)  opcode_R(CUSTOM0, 5, 0, (rs1), (rs2))
+#define cfu_op6_hw(rs1, rs2)  opcode_R(CUSTOM0, 6, 0, (rs1), (rs2))
+#define cfu_op7_hw(rs1, rs2)  opcode_R(CUSTOM0, 7, 0, (rs1), (rs2))
 
 
 
@@ -29,6 +33,11 @@ uint32_t Cfu(uint32_t functionid, uint32_t rs1, uint32_t rs2);
 #define cfu_op0_sw(rs1, rs2)  Cfu(0, rs1, rs2)
 #define cfu_op1_sw(rs1, rs2)  Cfu(1, rs1, rs2)
 #define cfu_op2_sw(rs1, rs2)  Cfu(2, rs1, rs2)
+#define cfu_op3_sw(rs1, rs2)  Cfu(3, rs1, rs2)
+#define cfu_op4_sw(rs1, rs2)  Cfu(4, rs1, rs2)
+#define cfu_op5_sw(rs1, rs2)  Cfu(5, rs1, rs2)
+#define cfu_op6_sw(rs1, rs2)  Cfu(6, rs1, rs2)
+#define cfu_op7_sw(rs1, rs2)  Cfu(7, rs1, rs2)
 
 
 
@@ -38,11 +47,21 @@ uint32_t Cfu(uint32_t functionid, uint32_t rs1, uint32_t rs2);
 #define cfu_op0(rs1, rs2)       cfu_op0_sw(rs1, rs2)
 #define cfu_op1(rs1, rs2)       cfu_op1_sw(rs1, rs2)
 #define cfu_op2(rs1, rs2)       cfu_op2_sw(rs1, rs2)
+#define cfu_op3(rs1, rs2)       cfu_op3_sw(rs1, rs2)
+#define cfu_op4(rs1, rs2)       cfu_op4_sw(rs1, rs2)
+#define cfu_op5(rs1, rs2)       cfu_op5_sw(rs1, rs2)
+#define cfu_op6(rs1, rs2)       cfu_op6_sw(rs1, rs2)
+#define cfu_op7(rs1, rs2)       cfu_op7_sw(rs1, rs2)
 
 #else
 
 #define cfu_op0(rs1, rs2)       cfu_op0_hw((rs1), (rs2))
 #define cfu_op1(rs1, rs2)       cfu_op1_hw((rs1), (rs2))
 #define cfu_op2(rs1, rs2)       cfu_op2_hw((rs1), (rs2))
+#define cfu_op3(rs1, rs2)       cfu_op3_hw((rs1), (rs2))
+#define cfu_op4(rs1, rs2)       cfu_op4_hw((rs1), (rs2))
+#define cfu_op5(rs1, rs2)       cfu_op5_hw((rs1), (rs2))
+#define cfu_op6(rs1, rs2)       cfu_op6_hw((rs1), (rs2))
+#define cfu_op7(rs1, rs2)       cfu_op7_hw((rs1), (rs2))
 
 #endif
