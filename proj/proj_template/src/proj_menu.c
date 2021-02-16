1b156c351bc72482a5557bf6c7a5eeb8f1e45d6e
Alan Green <avg@google.com>
2021-02-16 22:26:24 +1100

    proj_menu.c: incorporate cfu exercise
    Incorporate minimal example code showing CFU being exercised.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/proj_template/src/proj_menu.c
+++ proj/proj_template/src/proj_menu.c
@@ -15,18 +15,44 @@
  */
 
 #include <stdio.h>
+#include "cfu.h"
 #include "menu.h"
 #include "proj_menu.h"
 
 // Template Fn
-static void do_hello_world(void) {
+static void do_hello_world(void)
+{
     puts("Hello, World!!!\n");
+}
+
+// Test template instruction
+static void do_exercise_cfu_op0(void)
+{
+    puts("\nExercise CFU Op0 aka ADD\n");
+    int count = 0;
+    for (int a = -0x71234567; a < 0x68000000; a += 0x10012345)
+    {
+
+        for (int b = -0x7edcba98; b < 0x68000000; b += 0x10770077)
+        {
+            int cfu = cfu_op0(a, b);
+            printf("a: %08x b:%08x a+b=%08x cfu= %08x\n", a, b, a + b, cfu);
+            if (cfu != a + b) 
+            {
+                printf("\n***FAIL\n");
+                return;
+            }
+            count++;
+        }
+    }
+    printf("Performed %d comparisons", count);
 }
 
 static struct Menu MENU = {
     "Project Menu",
     "project",
     {
+        MENU_ITEM('0', "exercise cfu op0", do_exercise_cfu_op0),
         MENU_ITEM('h', "say Hello", do_hello_world),
         MENU_END,
     },
@@ -34,5 +60,5 @@ static struct Menu MENU = {
 
 void do_proj_menu()
 {
-  menu_run(&MENU);
+    menu_run(&MENU);
 }
