a81a509c182e03d0b8fde909660d524958bbbb3d
Alan Green <avg@google.com>
2021-02-17 10:09:04 +1100

    common: add functionality from basic_cfu
    Adds functionality from basic_cfu:
    
    1. Tests that are useful for functional (i.e non stateful) cfus. That
    said, the interactive test might also be useful for stateful cfus.
    
    2. Tests for performance counters.
    
    Signed-off-by: Alan Green <avg@google.com>

--- common/src/menu.h
+++ common/src/menu.h
@@ -14,33 +14,56 @@
  * limitations under the License.
  */
 
-
 #ifndef MENU_H
 #define MENU_H
 #include <stdbool.h>
 
-struct MenuItem {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Defines an item in a menu
+struct MenuItem
+{
+    // Character used to select the item
     const char selection;
+    // Description of the item in the menu
     const char *description;
-    const void (*fn)(void);
+    // The function that implements the menu item
+    void (*const fn)(void);
+    // Used to exit from submenus
     const bool exit;
 };
 
-struct Menu {
+// Defines a menu
+struct Menu
+{
+    // The title of the menu
     const char *title;
+    // String to use in the prompt
     const char *prompt;
+    // The menu choices
     struct MenuItem items[];
 };
 
-#define MENU_ITEM(selection, description, fn) {selection, description, fn, false}
+#define MENU_ITEM(selection, description, fn) \
+    {                                         \
+        selection, description, fn, false     \
+    }
 
-// Last item in a non-top level menu
+// Last items in a non-top level menu
 #define MENU_END {'x', "eXit to previous menu", NULL, true}, MENU_SENTINEL
 
 // Last item in a top level menu
-#define MENU_SENTINEL {'\0', NULL, NULL, false}
+#define MENU_SENTINEL           \
+    {                           \
+        '\0', NULL, NULL, false \
+    }
 
 // Run a menu
 void menu_run(struct Menu *menu);
 
+#ifdef __cplusplus
+}
+#endif
 #endif // MENU_H
