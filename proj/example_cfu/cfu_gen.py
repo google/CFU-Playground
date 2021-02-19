a15f5cc136dac848fbf01eafa470fa7aa8bcc7b6
Alan Green <avg@google.com>
2021-02-20 07:50:26 +1100

    example_cfu/cfu_gen.py: lazy cfu.v updating
    Copied cfu_gen.py from proj_template so that cfu.v is only updated when
    its content changes.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/example_cfu/cfu_gen.py
+++ proj/example_cfu/cfu_gen.py
@@ -1,11 +1,10 @@
-#!/bin/env python
-# Copyright 2020 Google LLC
+# Copyright 2021 The CFU-Playground Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os.path
 from nmigen import *
 from nmigen.back import rtlil, verilog
 
 from cfu import make_cfu
 
+VERILOG_FILENAME = "cfu.v"
+
+def read_file():
+    if os.path.exists(VERILOG_FILENAME):
+        with open(VERILOG_FILENAME, "r") as f:
+            return f.read()
+    return None
 
 def main():
     cfu = make_cfu()
-    with open("cfu.v", "w") as f:
-        f.write(verilog.convert(cfu, name='Cfu', ports=cfu.ports))
+    new_verilog = verilog.convert(cfu, name='Cfu', ports=cfu.ports)
+    old_verilog = read_file()
+    if new_verilog != old_verilog:
+        with open(VERILOG_FILENAME, "w") as f:
+            f.write(new_verilog)
 
 if __name__ == '__main__':
     main()
