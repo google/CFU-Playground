4a28c457935c43e448b123468a4d8b522cf4748e
Alan Green <avg@google.com>
2021-02-26 17:57:06 +1100

    conv.h: hand optimize
    Redo the hand optimizations since golden tests began to fail.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/avg_pdti8/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/avg_pdti8/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -33,18 +33,19 @@ namespace tflite
         int8_t *output_data)
     {
       // Get parameters.
-      const int32_t input_offset = 128;
-      const int32_t output_offset = -128;
+      const int32_t input_offset = params.input_offset; // r = s(q - Z)
+      const int32_t output_offset = params.output_offset;
 
       // Set min and max value of the output.
-      const int32_t output_activation_min = -128;
-      const int32_t output_activation_max = 127;
+      const int32_t output_activation_min = params.quantized_activation_min;
+      const int32_t output_activation_max = params.quantized_activation_max;
 
       // Consistency check.
       TFLITE_DCHECK_LE(output_activation_min, output_activation_max);
       TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
       TFLITE_DCHECK_EQ(filter_shape.DimensionsCount(), 4);
       TFLITE_DCHECK_EQ(output_shape.DimensionsCount(), 4);
+      const int batches = MatchingDim(input_shape, 0, output_shape, 0);
       const int input_depth = MatchingDim(input_shape, 3, filter_shape, 3);
       const int output_depth = MatchingDim(filter_shape, 0, output_shape, 3);
       if (bias_data)
@@ -53,44 +54,38 @@ namespace tflite
       }
 
       // Check dimensions of the tensors.
-      const int input_height = input_shape.Dims(1);
-      const int input_width = input_shape.Dims(2);
-      const int output_height = input_height;
-      const int output_width = input_width;
-
-      for (int out_y = 0; out_y < output_height; ++out_y)
+      const int output_height = output_shape.Dims(1);
+      const int output_width = output_shape.Dims(2);
+      for (int batch = 0; batch < batches; ++batch)
       {
-        for (int out_x = 0; out_x < output_width; ++out_x)
+        for (int out_y = 0; out_y < output_height; ++out_y)
         {
-          for (int out_channel = 0; out_channel < output_depth; ++out_channel)
+          for (int out_x = 0; out_x < output_width; ++out_x)
           {
-            perf_enable_counter(6);
-            int32_t acc = 0;
-            const int in_y = out_y;
-            const int in_x = out_x;
-
-            // No need to check zero padding
-
-            for (int in_channel = 0; in_channel < input_depth; ++in_channel)
+            for (int out_channel = 0; out_channel < output_depth; ++out_channel)
             {
-              int32_t input_val = input_data[Offset(input_shape, 0, in_y,
-                                                    in_x, in_channel)];
-              int32_t filter_val = filter_data[Offset(
-                  filter_shape, out_channel, 0, 0, in_channel)];
-              acc += filter_val * (input_val + input_offset);
-            }
-            perf_disable_counter(6);
-            perf_enable_counter(7);
+              int32_t acc = 0;
+              for (int in_channel = 0; in_channel < input_depth; ++in_channel)
+              {
+                int32_t input_val = input_data[Offset(input_shape, batch, out_y,
+                                                      out_x, in_channel)];
+                int32_t filter_val = filter_data[Offset(
+                    filter_shape, out_channel, 0, 0, in_channel)];
+                acc += filter_val * (input_val + input_offset);
+              }
 
-            acc += bias_data[out_channel];
-            acc = MultiplyByQuantizedMultiplier(
-                acc, output_multiplier[out_channel], output_shift[out_channel]);
-            acc += output_offset;
-            acc = std::max(acc, output_activation_min);
-            acc = std::min(acc, output_activation_max);
-            output_data[Offset(output_shape, 0, out_y, out_x, out_channel)] =
-                static_cast<int8_t>(acc);
-            perf_disable_counter(7);
+              if (bias_data)
+              {
+                acc += bias_data[out_channel];
+              }
+              acc = MultiplyByQuantizedMultiplier(
+                  acc, output_multiplier[out_channel], output_shift[out_channel]);
+              acc += output_offset;
+              acc = std::max(acc, output_activation_min);
+              acc = std::min(acc, output_activation_max);
+              output_data[Offset(output_shape, batch, out_y, out_x, out_channel)] =
+                  static_cast<int8_t>(acc);
+            }
           }
         }
       }
