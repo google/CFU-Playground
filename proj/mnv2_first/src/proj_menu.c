1198d2df4d49ce0054525648f4bc72bf9c45a30d
Alan Green <avg@google.com>
2021-03-06 11:09:21 +1100

    golden_op_tests: New golden op tests
    Uses values dumped from conv.cc to run a single 2D convolution and check
    the result. This has multiple benefits:
    
    (a) finer grained check on correctness than whole-of-model evaluation.
    (b) can more easily run this test in simulation.
    (c) can more easily run with software emulated CFU instructions.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/mnv2_first/src/proj_menu.c
+++ proj/mnv2_first/src/proj_menu.c
@@ -14,51 +14,39 @@
  * limitations under the License.
  */
 
-#include <stdio.h>
-#include "cfu.h"
-#include "menu.h"
 #include "proj_menu.h"
 
-// Template Fn
-static void do_hello_world(void)
-{
-    puts("Hello, World!!!\n");
+#include <stdio.h>
+#include <string.h>
+
+#include "b64_util.h"
+#include "cfu.h"
+#include "golden_op_tests.h"
+#include "menu.h"
+
+static void print_sample(char* s) {
+  printf("in = %s\n", s);
+  b64_dump(s, strlen(s));
 }
 
-// Test template instruction
-static void do_exercise_cfu_op0(void)
-{
-    puts("\nExercise CFU Op0 aka ADD\n");
-    int count = 0;
-    for (int a = -0x71234567; a < 0x68000000; a += 0x10012345)
-    {
-
-        for (int b = -0x7edcba98; b < 0x68000000; b += 0x10770077)
-        {
-            int cfu = cfu_op0(0, a, b);
-            printf("a: %08x b:%08x a+b=%08x cfu= %08x\n", a, b, a + b, cfu);
-            if (cfu != a + b) 
-            {
-                printf("\n***FAIL\n");
-                return;
-            }
-            count++;
-        }
-    }
-    printf("Performed %d comparisons", count);
+static void do_b64_samples() {
+  print_sample("1234");
+  // See https://en.wikipedia.org/wiki/Base64#Examples
+  print_sample(
+      "Man is distinguished, not only by his reason, but by this singular "
+      "passion from other animals, which is a lust of the mind, that by a "
+      "perseverance of delight in the continued and indefatigable generation "
+      "of knowledge, exceeds the short vehemence of any carnal pleasure.");
 }
 
 static struct Menu MENU = {
     "Project Menu",
-    "project",
+    "mnv2_first",
     {
-        MENU_ITEM('0', "exercise cfu op0", do_exercise_cfu_op0),
-        MENU_ITEM('h', "say Hello", do_hello_world),
+        MENU_ITEM('1', "1x1 conv2d golden tests", golden_op_run_1x1conv),
+        MENU_ITEM('b', "base64 samples", do_b64_samples),
         MENU_END,
     },
 };
 
-void do_proj_menu()
-{
-    menu_run(&MENU);
-}
+void do_proj_menu() { menu_run(&MENU); }
