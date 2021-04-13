5506e28203ab933cd0f9a7c2121625216d9ce332
Alan Green <avg@google.com>
2021-04-14 05:53:25 +1000

    stylesheets: add cfu-playground.css
    Adds a project-specific CSS file. The immediate motivation is to
    better highlight terms in definition lists.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,11 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+
+
+# CFU-Playground specific customisations
+html_css_files = ['stylesheets/cfu-playground.css']
 
 
 # -- Collect READMEs from examples --------------------------------------------
