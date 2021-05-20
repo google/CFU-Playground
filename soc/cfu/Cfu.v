8adf7fe11edb1b5901e8defd278de087cd47376d
Robert Szczepanski <rszczepanski@internships.antmicro.com>
2021-05-20 15:52:11 +0200

    cfu: remove io_bus_ prefix to match LiteX CFU integration

--- soc/cfu/Cfu.v
+++ soc/cfu/Cfu.v
@@ -15,49 +15,50 @@
 
 
 module Cfu (
-  input               io_bus_cmd_valid,
-  output              io_bus_cmd_ready,
-  input      [2:0]    io_bus_cmd_payload_function_id,
-  input      [31:0]   io_bus_cmd_payload_inputs_0,
-  input      [31:0]   io_bus_cmd_payload_inputs_1,
-  output              io_bus_rsp_valid,
-  input               io_bus_rsp_ready,
-  output              io_bus_rsp_payload_response_ok,
-  output     [31:0]   io_bus_rsp_payload_outputs_0,
-  input               clk
+  input               cmd_valid,
+  output              cmd_ready,
+  input      [2:0]    cmd_payload_function_id,
+  input      [31:0]   cmd_payload_inputs_0,
+  input      [31:0]   cmd_payload_inputs_1,
+  output              rsp_valid,
+  input               rsp_ready,
+  output              rsp_payload_response_ok,
+  output     [31:0]   rsp_payload_outputs_0,
+  input               clk,
+  input               reset
 );
 
-  assign io_bus_rsp_valid = io_bus_cmd_valid;
-  assign io_bus_cmd_ready = io_bus_rsp_ready;
-  assign io_bus_rsp_payload_response_ok = 1'b1;
+  assign rsp_valid = cmd_valid;
+  assign cmd_ready = rsp_ready;
+  assign rsp_payload_response_ok = 1'b1;
 
   //  byte sum (unsigned)
   wire [31:0] cfu0;
-  assign cfu0[31:0] =  io_bus_cmd_payload_inputs_0[7:0]   + io_bus_cmd_payload_inputs_1[7:0] +
-                       io_bus_cmd_payload_inputs_0[15:8]  + io_bus_cmd_payload_inputs_1[15:8] +
-                       io_bus_cmd_payload_inputs_0[23:16] + io_bus_cmd_payload_inputs_1[23:16] +
-                       io_bus_cmd_payload_inputs_0[31:24] + io_bus_cmd_payload_inputs_1[31:24];
+  assign cfu0[31:0] =  cmd_payload_inputs_0[7:0]   + cmd_payload_inputs_1[7:0] +
+                       cmd_payload_inputs_0[15:8]  + cmd_payload_inputs_1[15:8] +
+                       cmd_payload_inputs_0[23:16] + cmd_payload_inputs_1[23:16] +
+                       cmd_payload_inputs_0[31:24] + cmd_payload_inputs_1[31:24];
 
   // byte swap
   wire [31:0] cfu1;
-  assign cfu1[31:24] =     io_bus_cmd_payload_inputs_0[7:0];
-  assign cfu1[23:16] =     io_bus_cmd_payload_inputs_0[15:8];
-  assign cfu1[15:8] =      io_bus_cmd_payload_inputs_0[23:16];
-  assign cfu1[7:0] =       io_bus_cmd_payload_inputs_0[31:24];
+  assign cfu1[31:24] =     cmd_payload_inputs_0[7:0];
+  assign cfu1[23:16] =     cmd_payload_inputs_0[15:8];
+  assign cfu1[15:8] =      cmd_payload_inputs_0[23:16];
+  assign cfu1[7:0] =       cmd_payload_inputs_0[31:24];
 
   // bit reverse
   wire [31:0] cfu2;
   genvar n;
   generate
       for (n=0; n<32; n=n+1) begin
-          assign cfu2[n] =     io_bus_cmd_payload_inputs_0[31-n];
+          assign cfu2[n] =     cmd_payload_inputs_0[31-n];
       end
   endgenerate
 
 
   // select output
-  assign io_bus_rsp_payload_outputs_0 = io_bus_cmd_payload_function_id[1] ? cfu2 :
-                                      ( io_bus_cmd_payload_function_id[0] ? cfu1 : cfu0);
+  assign rsp_payload_outputs_0 = cmd_payload_function_id[1] ? cfu2 :
+                                      ( cmd_payload_function_id[0] ? cfu1 : cfu0);
 
 
 endmodule
