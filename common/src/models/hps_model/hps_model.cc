954b8d7dd3da420df54a90751ea567582846ad0f
Alan Green <avg@google.com>
2021-07-17 08:31:37 +1000

    hps_model: New model and test cases
    Replaces the model and test cases for hps_model.
    
    - output vector is 1 value instead of two
    - replace synthetic input test cases with image input
    - includes two versions of model - one tiled and one untiled

--- common/src/models/hps_model/hps_model.cc
+++ common/src/models/hps_model/hps_model.cc
@@ -17,48 +17,53 @@
 #include <stdio.h>
 
 #include "menu.h"
-#include "models/hps_model/hps_model_2021_07_05.h"
+#include "models/hps_model/cat_picture.h"
+#include "models/hps_model/diagram.h"
+#include "models/hps_model/hps_model_2021_07_05_tiled.h"
+#include "models/hps_model/hps_model_2021_07_05_untiled.h"
 #include "tflite.h"
 
 namespace {
 
-// Initialize everything once
-// deallocate tensors when done
-void init(void) {
-  tflite_load_model(hps_model_2021_07_05, hps_model_2021_07_05_len);
+// Initialize with the tiled version
+void init_tiled(void) {
+  puts("Loading Tiled HPS model");
+  tflite_load_model(hps_model_2021_07_05_tiled, hps_model_2021_07_05_tiled_len);
 }
 
-// Run classification, after input has been loaded
-int32_t classify(const char* desc) {
-  printf("Running %s\n", desc);
-  tflite_classify();
+// Initialize with the untiled version
+void init_untiled(void) {
+  puts("Loading Untiled HPS model");
+  tflite_load_model(hps_model_2021_07_05_untiled, hps_model_2021_07_05_untiled_len);
+}
 
-  // Process the inference results.
+// Run classification and interpret results
+int32_t classify() {
+  tflite_classify();
   int8_t* output = tflite_get_output();
-  return (int32_t)output[1] - (int32_t)output[0];
+  return output[0];
 }
 
 // Classify with random data
-int32_t classify_random_1() {
-  constexpr int64_t INPUT_SEED_1 = 0x1234567890abcdef;
-  tflite_randomize_input(INPUT_SEED_1);
-  return classify("random 1");
+int32_t classify_cat() {
+  tflite_set_input_unsigned(cat_picture);
+  return classify();
 }
 
-void do_classify_random_1() { printf("Result is %ld\n", classify_random_1()); }
+void do_classify_cat() { printf("Result is %ld\n", classify_cat()); }
 
 // Classify with a grid pattern
-int32_t classify_grid() {
-  tflite_set_grid_input();
-  return classify("grid");
+int32_t classify_diagram() {
+  tflite_set_input_unsigned(diagram);
+  return classify();
 }
 
-void do_classify_grid() { printf("Result is %ld\n", classify_grid()); }
+void do_classify_diagram() { printf("Result is %ld\n", classify_diagram()); }
 
 // Set input to zero and run classification
 int32_t classify_zeros() {
   tflite_set_input_zeros();
-  return classify("zeroed input");
+  return classify();
 }
 
 void do_classify_zeros() { printf("Result is %ld\n", classify_zeros()); }
@@ -66,25 +71,28 @@ void do_classify_zeros() { printf("Result is %ld\n", classify_zeros()); }
 // Golden tests: expected results
 struct GoldenTest {
   int32_t (*fn)();
+  const char* name;
   int32_t expected;
 };
 
 GoldenTest golden_tests[4] = {
-    {classify_random_1, 0},
-    {classify_grid, -3},
-    {classify_zeros, -1},
-    {nullptr, 0},
+    {classify_cat, "cat", -116},
+    {classify_diagram, "diagram", -124},
+    {classify_zeros, "zeroes", -123},
+    {nullptr, "", 0},
 };
 
 static void do_golden_tests() {
   bool failed = false;
   for (size_t i = 0; golden_tests[i].fn; i++) {
+    printf("Testing input %s: ", golden_tests[i].name);
     int32_t actual = golden_tests[i].fn();
     int32_t expected = golden_tests[i].expected;
     if (actual != expected) {
       failed = true;
-      printf("*** Golden test %d failed: %ld (actual) != %ld (expected))\n", i,
-             actual, expected);
+      printf("FAIL %ld (actual) != %ld (expected) ***\n", actual, expected);
+    } else {
+      printf("OK\n");
     }
   }
 
@@ -96,13 +104,15 @@ static void do_golden_tests() {
 }
 
 struct Menu MENU = {
-    "Tests for HPS model model",
+    "Tests for HPS model",
     "hps",
     {
-        MENU_ITEM('1', "Run with random input seed #1", do_classify_random_1),
-        MENU_ITEM('2', "Run with grid", do_classify_grid),
-        MENU_ITEM('z', "Run with zeros input", do_classify_zeros),
-        MENU_ITEM('g', "Run golden tests (check for expected outputs)",
+        MENU_ITEM('t', "reinitialize with Tiled model", init_tiled),
+        MENU_ITEM('u', "reinitialize with Untiled model", init_untiled),
+        MENU_ITEM('c', "Cat picture input", do_classify_cat),
+        MENU_ITEM('d', "Diagram input", do_classify_diagram),
+        MENU_ITEM('z', "Zeros input", do_classify_zeros),
+        MENU_ITEM('g', "Golden tests (check for expected outputs)",
                   do_golden_tests),
         MENU_END,
     },
@@ -112,6 +122,6 @@ struct Menu MENU = {
 
 // For integration into menu system
 extern "C" void hps_model_menu() {
-  init();
+  init_tiled();
   menu_run(&MENU);
 }
