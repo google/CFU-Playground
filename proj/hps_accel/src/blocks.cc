26a7576898b87ce3ec23b874c3dc9f62c863d8f6
Alan Green <avg@google.com>
2021-07-24 08:33:14 +1000

    hps_accel/blocks: Force software CFU for get/set
    Force software CFU for all get and set instructions used by blocks,
    since there is no appropriate hardware implementation, yet.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/hps_accel/src/blocks.cc
+++ proj/hps_accel/src/blocks.cc
@@ -36,12 +36,12 @@ inline uint32_t pack32(int8_t a, int8_t b, int8_t c, int8_t d) {
 // Load input when in_channels = 1
 void LoadInput1(size_t width, const int8_t* values) {
   size_t input_words = 4;
-  cfu_set(REG_INPUT_NUM_WORDS, input_words);
+  cfu_set_sw(REG_INPUT_NUM_WORDS, input_words);
 
   // For each of four consecutive rows, take data from four consecutive pixels
   for (size_t row_index = 0; row_index < 4; row_index++) {
     const int8_t* source = values + (row_index * width);
-    cfu_set(REG_SET_INPUT, pack32(source[0], source[1], source[2], source[3]));
+    cfu_set_sw(REG_SET_INPUT, pack32(source[0], source[1], source[2], source[3]));
   }
 }
 
@@ -53,14 +53,14 @@ void LoadInputN(size_t width, size_t in_channels, const int8_t* values) {
   const size_t bytes_per_width = width * in_channels;
   size_t input_words =
       FILTER_WIDTH * FILTER_HEIGHT * in_channels / BYTES_PER_WORD;
-  cfu_set(REG_INPUT_NUM_WORDS, input_words);
+  cfu_set_sw(REG_INPUT_NUM_WORDS, input_words);
 
   // For each of four consecutive rows, take data from four consecutive pixels
   for (size_t row_index = 0; row_index < 4; row_index++) {
     const uint32_t* source = reinterpret_cast<const uint32_t*>(
         values + (row_index * bytes_per_width));
     for (size_t i = 0; i < words_per_input_row; i++) {
-      cfu_set(REG_SET_INPUT, *source++);
+      cfu_set_sw(REG_SET_INPUT, *source++);
     }
   }
 }
@@ -80,34 +80,34 @@ Vector16 Vector16::zeroes() { return Vector16{{0, 0, 0, 0}}; }
 // Performs a 16 x 16 vector multiplication
 int32_t multiply_accumulate(Vector16 input, Vector16 filter,
                             int32_t input_offset) {
-  cfu_set(REG_INPUT_OFFSET, input_offset);
-  cfu_set(REG_MACC_INPUT_0, input.values[0]);
-  cfu_set(REG_MACC_INPUT_1, input.values[1]);
-  cfu_set(REG_MACC_INPUT_2, input.values[2]);
-  cfu_set(REG_MACC_INPUT_3, input.values[3]);
-  cfu_set(REG_MACC_FILTER_0, filter.values[0]);
-  cfu_set(REG_MACC_FILTER_1, filter.values[1]);
-  cfu_set(REG_MACC_FILTER_2, filter.values[2]);
-  cfu_set(REG_MACC_FILTER_3, filter.values[3]);
-  return cfu_get(REG_MACC_OUT);
+  cfu_set_sw(REG_INPUT_OFFSET, input_offset);
+  cfu_set_sw(REG_MACC_INPUT_0, input.values[0]);
+  cfu_set_sw(REG_MACC_INPUT_1, input.values[1]);
+  cfu_set_sw(REG_MACC_INPUT_2, input.values[2]);
+  cfu_set_sw(REG_MACC_INPUT_3, input.values[3]);
+  cfu_set_sw(REG_MACC_FILTER_0, filter.values[0]);
+  cfu_set_sw(REG_MACC_FILTER_1, filter.values[1]);
+  cfu_set_sw(REG_MACC_FILTER_2, filter.values[2]);
+  cfu_set_sw(REG_MACC_FILTER_3, filter.values[3]);
+  return cfu_get_sw(REG_MACC_OUT);
 }
 
 void LoadFilter(size_t in_channels, size_t out_channels, const int8_t* values) {
   const uint32_t* values_as_words = reinterpret_cast<const uint32_t*>(values);
   size_t filter_words =
       FILTER_WIDTH * FILTER_HEIGHT / 4 * in_channels * out_channels;
-  cfu_set(REG_FILTER_NUM_WORDS, filter_words);
+  cfu_set_sw(REG_FILTER_NUM_WORDS, filter_words);
   const uint32_t* end = values_as_words + filter_words;
   while (values_as_words != end) {
-    cfu_set(REG_SET_FILTER, *values_as_words++);
+    cfu_set_sw(REG_SET_FILTER, *values_as_words++);
   }
 }
 
 Vector16 GetFilter() {
-  uint32_t word0 = cfu_get(REG_FILTER_0);
-  uint32_t word1 = cfu_get(REG_FILTER_1);
-  uint32_t word2 = cfu_get(REG_FILTER_2);
-  uint32_t word3 = cfu_get(REG_FILTER_3);
+  uint32_t word0 = cfu_get_sw(REG_FILTER_0);
+  uint32_t word1 = cfu_get_sw(REG_FILTER_1);
+  uint32_t word2 = cfu_get_sw(REG_FILTER_2);
+  uint32_t word3 = cfu_get_sw(REG_FILTER_3);
   return Vector16{{word0, word1, word2, word3}};
 }
 
@@ -122,10 +122,10 @@ void LoadInput(size_t width, size_t in_channels, const int8_t* values) {
 }
 
 Vector16 GetInput() {
-  uint32_t word0 = cfu_get(REG_INPUT_0);
-  uint32_t word1 = cfu_get(REG_INPUT_1);
-  uint32_t word2 = cfu_get(REG_INPUT_2);
-  uint32_t word3 = cfu_get(REG_INPUT_3);
+  uint32_t word0 = cfu_get_sw(REG_INPUT_0);
+  uint32_t word1 = cfu_get_sw(REG_INPUT_1);
+  uint32_t word2 = cfu_get_sw(REG_INPUT_2);
+  uint32_t word3 = cfu_get_sw(REG_INPUT_3);
   return Vector16{{word0, word1, word2, word3}};
 }
 
