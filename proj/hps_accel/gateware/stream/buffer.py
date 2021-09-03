bc4547e5557216753a1ef8d8179c9421ba698e69
Dan Callaghan <dcallagh@google.com>
2021-09-03 16:00:57 +1000

    hps_accel: introduce ConcatenatingBuffer
    Can be used to combine the 4x input streams and 4x filter streams into
    a single stream that presents all operands to the macc at once.
    
    Signed-off-by: Dan Callaghan <dcallagh@google.com>

--- proj/hps_accel/gateware/stream/buffer.py
+++ proj/hps_accel/gateware/stream/buffer.py
@@ -14,6 +14,8 @@
 
 from nmigen_cfu.util import SimpleElaboratable
 from nmigen import Value, Module, Signal, Mux
+from nmigen.hdl.ast import Cat
+from nmigen.hdl.rec import Layout
 
 from .stream import Endpoint
 
@@ -82,3 +84,90 @@ class Buffer(SimpleElaboratable):
         with m.If(self.reset):
             m.d.sync += buffering.eq(False)
             m.d.sync += buffered_value.eq(0)
+
+
+class ConcatenatingBuffer(SimpleElaboratable):
+    """A buffer which concatenates multiple input streams.
+
+    Passes multiple input streams through to a single output, with their
+    payloads concatenated. Given two input streams, the first with payload
+    A and the second with payload B, the output stream will have payload BA.
+
+    The output stream only transfers when all inputs are valid. If input values
+    arrive at different times, they are stored until a complete valid value
+    can be presented at the output.
+
+    Parameters
+    ----------
+
+    input_streams: list[(str, Shape)]
+      Name and payload shape for each input stream.
+
+    Attributes
+    ----------
+
+    inputs: {name: Endpoint}, in
+      Inbound streams, keyed on name.
+
+    output: Endpoint, out
+      Outbound stream.
+
+    reset: Signal
+      Set high to reset this buffer. Set low to enable buffer.
+    """
+
+    def __init__(self, input_streams):
+        self.field_names = list(name for name, shape in input_streams)
+        self.field_shapes = {name: shape for name, shape in input_streams}
+        self.inputs = {name: Endpoint(shape) for name, shape in input_streams}
+        self.output = Endpoint(Layout(input_streams))
+        self.reset = Signal()
+
+    def elab(self, m: Module):
+        # One signal for each input stream, indicating whether
+        # there is a value being buffered:
+        buffering = {name: Signal(name=f'buffering_{name}')
+                     for name in self.field_names}
+        # A buffer for each input stream:
+        buffered_values = \
+            {name: Signal(self.field_shapes[name], name=f'buffered_{name}')
+             for name in self.field_names}
+
+        # For each field of the concatenated output, present either the
+        # buffered value if we have one, or else plumb through the input.
+        for name in self.field_names:
+            m.d.comb += self.output.payload[name].eq(
+                    Mux(buffering[name],
+                        buffered_values[name],
+                        self.inputs[name].payload))
+
+        # The output is valid if we have either a buffered value or a valid
+        # input for every slice in the output.
+        valid_or_buffering = (Cat(*[ep.valid for ep in self.inputs.values()]) |
+                              Cat(*buffering.values()))
+        m.d.comb += self.output.valid.eq(valid_or_buffering.all())
+
+        for name, input in self.inputs.items():
+            # We can accept an input if the buffer is not occupied,
+            # or if we can output this cycle.
+            m.d.comb += input.ready.eq(~buffering[name] |
+                                       self.output.is_transferring())
+
+            with m.If(input.valid):
+                # Buffer it if the buffer is not occupied and we can't output.
+                with m.If(~buffering[name] & ~self.output.is_transferring()):
+                    m.d.sync += buffering[name].eq(True)
+                    m.d.sync += buffered_values[name].eq(input.payload)
+                # Buffer it if the buffer is occupied but we are outputting.
+                with m.If(self.output.is_transferring()):
+                    m.d.sync += buffered_values[name].eq(input.payload)
+            with m.Else():
+                with m.If(self.output.is_transferring()):
+                    m.d.sync += buffering[name].eq(False)
+
+        # Reset all state
+        with m.If(self.reset):
+            for b in buffering.values():
+                m.d.sync += b.eq(False)
+            for bv in buffered_values.values():
+                m.d.sync += bv.eq(0)
--- proj/hps_accel/gateware/stream/test_buffer.py
+++ proj/hps_accel/gateware/stream/test_buffer.py
@@ -14,12 +14,13 @@
 # limitations under the License.
 
 from nmigen import unsigned
-from nmigen.sim.core import Delay
+from nmigen.hdl.rec import Layout
+from nmigen.sim.core import Delay, Settle
 
 
 from nmigen_cfu.util import TestBase
 
-from .buffer import Buffer
+from .buffer import Buffer, ConcatenatingBuffer
 
 
 class TestBuffer(TestBase):
@@ -48,6 +49,7 @@ class TestBuffer(TestBase):
             # Single value, delay ready two cycle
             ((0, 0, 1), (0, None, 0)),
             ((1, 22, 1), (1, 22, 0)),
+            ((0, 0, 0), (1, 22, 0)),
             ((0, 0, 1), (1, 22, 1)),
             ((0, 0, 1), (0, None, 1)),
             # Buffer a stream of values
@@ -79,3 +81,89 @@ class TestBuffer(TestBase):
                                      expected_out_payload)
                 yield
         self.run_sim(process, False)
+
+
+class TestConcatenatingBuffer(TestBase):
+
+    def create_dut(self):
+        return ConcatenatingBuffer([('x', unsigned(8)), ('y', unsigned(8))])
+
+    def test(self):
+        DATA = [
+            # Format is: input x (valid, payload, ready),
+            #            input y (valid, payload, ready),
+            #            output (valid, payload, ready)
+            # Simultaneous inputs, passthrough, output constant ready
+            ((0, 0, 1), (0, 0, 1), (0, None, 1)),
+            ((1, 0xaa, 1), (1, 0xbb, 1), (1, 0xbbaa, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 1)),
+            # Simultaneous inputs, passthrough, output instantaneosly ready
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            ((1, 0xcc, 1), (1, 0xdd, 1), (1, 0xddcc, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # Simultaneous inputs, delay ready one cycle
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            ((1, 0xee, 1), (1, 0xff, 1), (1, 0xffee, 0)),
+            ((0, 0, 1), (0, 0, 1), (1, 0xffee, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # Simultaneous inputs, delay ready two cycle
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            ((1, 0x77, 1), (1, 0x88, 1), (1, 0x8877, 0)),
+            ((0, 0, 0), (0, 0, 0), (1, 0x8877, 0)),
+            ((0, 0, 1), (0, 0, 1), (1, 0x8877, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # Buffer a stream of simultaneous inputs
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            ((1, 0x11, 1), (1, 0x22, 1), (1, 0x2211, 0)),
+            ((1, 0x33, 0), (1, 0x44, 0), (1, 0x2211, 0)),
+            ((1, 0x33, 0), (1, 0x44, 0), (1, 0x2211, 0)),
+            ((1, 0x33, 1), (1, 0x44, 1), (1, 0x2211, 1)),
+            ((1, 0x55, 1), (1, 0x66, 1), (1, 0x4433, 1)),
+            ((1, 0x77, 1), (1, 0x88, 1), (1, 0x6655, 1)),
+            ((0, 0, 0), (0, 0, 0), (1, 0x8877, 0)),
+            ((0, 0, 1), (0, 0, 1), (1, 0x8877, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # One input arrives before the other, passthrough same cycle
+            ((1, 0xaa, 1), (0, 0, 1), (0, None, 0)),
+            ((0, 0, 1), (1, 0xbb, 1), (1, 0xbbaa, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # One input arrives before the other, both buffered
+            ((1, 0xaa, 1), (0, 0, 1), (0, None, 0)),
+            ((0, 0, 0), (1, 0xbb, 1), (1, 0xbbaa, 0)),
+            ((0, 0, 1), (0, 0, 1), (1, 0xbbaa, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+            # One input stream starts before the other
+            ((1, 0x11, 1), (0, 0, 1), (0, None, 0)),
+            ((1, 0x33, 0), (1, 0x22, 1), (1, 0x2211, 0)),
+            ((1, 0x33, 0), (1, 0x44, 0), (1, 0x2211, 0)),
+            ((1, 0x33, 1), (1, 0x44, 1), (1, 0x2211, 1)),
+            ((1, 0x55, 1), (1, 0x66, 1), (1, 0x4433, 1)),
+            ((1, 0x77, 1), (1, 0x88, 1), (1, 0x6655, 1)),
+            ((0, 0, 0), (0, 0, 0), (1, 0x8877, 0)),
+            ((0, 0, 1), (0, 0, 1), (1, 0x8877, 1)),
+            ((0, 0, 1), (0, 0, 1), (0, None, 0)),
+        ]
+
+        def process():
+            dut = self.dut
+            for n, (input_x, input_y, outputs) in enumerate(DATA):
+                in_x_valid, in_x_payload, expected_in_x_ready = input_x
+                in_y_valid, in_y_payload, expected_in_y_ready = input_y
+                expected_out_valid, expected_out_payload, out_ready = outputs
+                yield dut.inputs['x'].valid.eq(in_x_valid)
+                yield dut.inputs['x'].payload.eq(in_x_payload)
+                yield dut.inputs['y'].valid.eq(in_y_valid)
+                yield dut.inputs['y'].payload.eq(in_y_payload)
+                yield dut.output.ready.eq(out_ready)
+                yield Settle()
+                self.assertEqual((yield dut.inputs['x'].ready),
+                                 expected_in_x_ready)
+                self.assertEqual((yield dut.inputs['y'].ready),
+                                 expected_in_y_ready)
+                self.assertEqual((yield dut.output.valid), expected_out_valid)
+                if expected_out_payload is not None:
+                    self.assertEqual(
+                            hex((yield dut.output.payload.as_value())),
+                            hex(expected_out_payload))
+                yield
+        self.run_sim(process)
