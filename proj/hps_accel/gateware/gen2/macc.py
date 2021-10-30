277086450e1776a3b18c8f2e4dfaa52298ea3a3b
Alan Green <avg@google.com>
2021-10-30 18:35:13 +1100

    gen2/macc: Multiply Accumulate block for hps_accel
    A multiply accumulate block for the hps_accel gen2 accelerator.
    
    Signed-off-by: Alan Green <avg@google.com>

---
+++ proj/hps_accel/gateware/gen2/macc.py
@@ -0,0 +1,162 @@
+#!/bin/env python
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Multiply Accumulate Blocks for a systolic array"""
+
+
+from nmigen import Mux, Signal, unsigned
+from nmigen_cfu.util import tree_sum, SimpleElaboratable
+
+
+class MaccBlock(SimpleElaboratable):
+    """An N-wide multiply and accumulate block.
+
+    This is intended to be used as a block in a systolic array. It has
+    two inputs, A and B. A is input from the left and is passed to the
+    right. B is input from the top and passed to the bottom.
+
+    The calcuation performed is:
+
+    accumulator += sum(A[n] * B[n] for n in range N)
+
+    This block provides no flow control mechanisms. It is expected that
+    new inputs be provided on every cycle, and that output will be
+    processed when it is available. Avoiding flow control mechanisms
+    reduces the complexity and area use of this component, at the cost
+    of some additional external complexity.
+
+    The A input also carries 'first' and 'last signals' which control
+    accumulation.
+
+    This logic is intended to map to the Crosslink/NX SysDSP block.
+
+    Parameters
+    ----------
+
+    n: int
+        The number of multipliers. Four is the usual vavlue.
+
+    a_shape: Shape
+        The shape of each the value packed into the A input. If the A
+        input is used for Conv2D activation values, signed(9) or
+        unsigned(8) might be appropriate values.
+
+    b_shape: Shape
+        The shape of each the value packed into the B input. If the B
+        input is used for Conv2D filter values, signed(8) would be the
+        appropriate shape.
+
+    accumulator_shape: Shape
+        The shape of the accumulator. The accumulator should have
+        enough precision that it does not overflow under normal
+        operation. signed(32) is usually large enough, but a narrower
+        accumulator may be sufficient, and will make more efficient
+        use of the FPGA fabric.
+
+    Attributes
+    ----------
+
+    input_a: unsigned(n * a_shape.width), in
+        The input data containing A values.
+    output_a: unsigned(n * a_shape.width), out
+        The value of input_a, delayed by one cycle.
+    input_b: unsigned(n * b_shape.width), in
+        The input data containing B values.
+    output_b: unsigned(n * b_shape.width), out
+        The value of input_b, delayed by one cycle.
+    input_first: Signal(1), in
+        Indicates that this is the first value passed, and the
+        accumulator should be assumed zero.
+    output_first: Signal(1), out
+        The value of input_first, delayed by one cycle
+    output_last: Signal(1), out
+        The value of output_last, delayed by one cycle
+    output_accumulator: accumulator_shape, out
+        Holds the output value. Since there is no flow control, this
+        value must be read before it is updated again.
+    """
+
+    # The latency from input_received to accumulator being updated
+    # Moving the accumulator to accumulator_output takes an additional
+    # cycle.
+    PIPELINE_CYCLES = 2
+
+    def __init__(self, n, a_shape, b_shape, accumulator_shape):
+        self._n = n
+        self._a_shape = a_shape
+        self._b_shape = b_shape
+        self._accumulator_shape = accumulator_shape
+
+        self.input_a = Signal(unsigned(n * a_shape.width))
+        self.output_a = Signal.like(self.input_a)
+        self.input_b = Signal(unsigned(n * b_shape.width))
+        self.output_b = Signal.like(self.input_b)
+
+        self.input_first = Signal()
+        self.output_first = Signal()
+        self.input_last = Signal()
+        self.output_last = Signal()
+
+        self.output_accumulator = Signal(accumulator_shape)
+
+    def _connect_passthrough(self, m):
+        """Connects the block pass through signals."""
+        m.d.sync += [
+            self.output_a.eq(self.input_a),
+            self.output_b.eq(self.input_b),
+            self.output_first.eq(self.input_first),
+            self.output_last.eq(self.input_last),
+        ]
+
+    @staticmethod
+    def _delay(m, sig, cycles):
+        """Creates simple shift register that delays a bit signal."""
+        shift_register = Signal(cycles)
+        m.d.sync += [
+            shift_register[1:].eq(shift_register[0:]),
+            shift_register[0].eq(sig)
+        ]
+        return shift_register
+
+    def elab(self, m):
+        self._connect_passthrough(m)
+
+        # Pipeline cycle 0: calculate products
+        products = []
+        for i in range(self._n):
+            a_bits = self.input_a.word_select(i, self._a_shape.width)
+            b_bits = self.input_b.word_select(i, self._b_shape.width)
+            a = Signal(self._a_shape)
+            b = Signal(self._b_shape)
+            m.d.comb += [
+                a.eq(a_bits),
+                b.eq(b_bits),
+            ]
+            ab = Signal.like(a * b)
+            m.d.sync += ab.eq(a * b)
+            products.append(ab)
+
+        # Pipeline cycle 1: accumulate
+        product_sum = Signal.like(tree_sum(products))
+        m.d.comb += product_sum.eq(tree_sum(products))
+        first_delayed = self._delay(m, self.input_first, 1)
+        accumulator = Signal(self._accumulator_shape)
+        base = Mux(first_delayed[0], 0, accumulator)
+        m.d.sync += accumulator.eq(base + product_sum)
+
+        # Pipeline cycle 2: optional accumulator output
+        last_delayed = self._delay(m, self.input_last, 2)
+        with m.If(last_delayed[1]):
+            m.d.sync += self.output_accumulator.eq(accumulator)
---
+++ proj/hps_accel/gateware/gen2/test_macc.py
@@ -0,0 +1,125 @@
+#!/bin/env python
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for macc.py"""
+
+import random
+
+from nmigen import unsigned, signed
+from nmigen.sim import Delay
+
+from nmigen_cfu import TestBase
+
+from .macc import MaccBlock
+
+
+class MaccBlockTest(TestBase):
+    """Tests MaccBlock"""
+
+    def create_dut(self):
+        return MaccBlock(4, unsigned(8), signed(8), signed(24))
+
+    def test_basic_functions(self):
+        DATA = [
+            # inputs: a, b, first, last
+            # expected: accumulator output
+            ((0, 0, 0, 0), None),
+            ((0, 0, 0, 0), 0),
+
+            # Multiply two sets of numbers (1 byte only)
+            ((1, -2, 1, 0), 0),  # -2
+            ((5, 3, 0, 1), 0),  # +15
+            ((6, 26, 0, 0), 0),  # parameters should not affect output
+            ((2, 17, 0, 0), 0),
+            ((4, 21, 0, 0), 13),
+
+            # Four sets of four numbers - result calculated by hand
+            ((0x01020304, 0x05060708, 1, 0), 13),
+            ((0x0a0b0c0d, 0xfffefdfc, 0, 0), 13),  # filter values negative
+            ((0x18191a1b, 0x1113171d, 0, 0), 13),
+            ((0x22232425, 0x1b1d2127, 0, 1), 13),
+            ((6, 26, 0, 0), 13),  # parameters should not affect output
+            ((2, 17, 0, 0), 13),
+            ((4, 21, 0, 0), 6778),
+        ]
+
+        def process():
+            prev_a, prev_b, prev_first, prev_last = None, None, None, None
+            for (a, b, first, last), expected_accumulator in DATA:
+                yield self.dut.input_a.eq(a)
+                yield self.dut.input_b.eq(b)
+                yield self.dut.input_first.eq(first)
+                yield self.dut.input_last.eq(last)
+                yield Delay(0.1)
+                if prev_a is not None:
+                    self.assertEqual((yield self.dut.output_a), prev_a & 0xffff_ffff)
+                if prev_b is not None:
+                    self.assertEqual((yield self.dut.output_b), prev_b & 0xffff_ffff)
+                if prev_first is not None:
+                    self.assertEqual((yield self.dut.output_first), prev_first)
+                if prev_last is not None:
+                    self.assertEqual((yield self.dut.output_last), prev_last)
+                if expected_accumulator is not None:
+                    self.assertEqual((yield self.dut.output_accumulator), expected_accumulator)
+                yield
+                prev_a, prev_b, prev_first, prev_last = a, b, first, last
+
+        self.run_sim(process, False)
+
+    def check_random_calculation(self, size, seed):
+        """Checks a randomly generated calculation.
+
+        Args:
+          size - number of arguments must be divisble by 4
+          seed - used to seed the generator.
+        """
+        random.seed(seed)
+        a_list = [random.randrange(0, 256) for _ in range(size)]
+        b_list = [random.randrange(-128, 128) for _ in range(size)]
+        expected_result = sum(a * b for a, b in zip(a_list, b_list))
+
+        def to_word(x, y, z, t):
+            return ((x & 0xff) |
+                    ((y & 0xff) << 8) |
+                    ((z & 0xff) << 16) |
+                    ((t & 0xff) << 24))
+
+        def process():
+            # Send in all inputs
+            num_inputs = size // 4
+            for i in range(num_inputs):
+                a = to_word(*a_list[i * 4: (i + 1) * 4])
+                b = to_word(*b_list[i * 4: (i + 1) * 4])
+                yield self.dut.input_a.eq(a)
+                yield self.dut.input_b.eq(b)
+                yield self.dut.input_first.eq(i == 0)
+                yield self.dut.input_last.eq(i == (num_inputs - 1))
+                yield
+            # wait for output to be available
+            yield
+            yield
+            yield
+            self.assertEqual((yield self.dut.output_accumulator), expected_result)
+
+        return process()
+
+    def test_larger_calculations(self):
+        def process():
+            yield from self.check_random_calculation(32, 1)
+            yield from self.check_random_calculation(500, 2)
+            yield from self.check_random_calculation(64, 3)
+            yield from self.check_random_calculation(48, 3)
+
+        self.run_sim(process, False)
