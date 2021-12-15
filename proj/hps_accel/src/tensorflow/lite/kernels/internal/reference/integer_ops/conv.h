01aad3a5743b0b88d0564047f465472ee46f7308
Alan Green <avg@google.com>
2021-12-15 13:17:36 +1100

    hps_accel/conv.h: move capture to end of file
    Move data capture back to end of file, after output data are calculated.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/hps_accel/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/hps_accel/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -77,36 +77,6 @@ inline void ConvPerChannel(
   printf("bias\n");
   print_int32_array(bias_data, output_shape.Dims(3));
 #endif
-#ifdef DUMP_LAYER
-  static int invocation_count = 0;
-  if (invocation_count == DUMP_LAYER) {
-    printf("\n\n");
-    // const ConvParams& params,
-    dump_c_array("params", &params, sizeof(ConvParams));
-
-    // const int32_t* output_multiplier, const int32_t* output_shift,
-    dump_c_array("output_multiplier", output_multiplier, 4 * output_depth);
-    dump_c_array("output_shift", output_shift, 4 * output_depth);
-
-    // const RuntimeShape& input_shape, const int8_t* input_data,
-    dump_c_array("input_shape", &input_shape, sizeof(RuntimeShape));
-    dump_c_array("input_data", input_data, input_shape.FlatSize());
-
-    // const RuntimeShape& filter_shape, const int8_t* filter_data,
-    dump_c_array("filter_shape", &filter_shape, sizeof(RuntimeShape));
-    dump_c_array("filter_data", filter_data, filter_shape.FlatSize());
-
-    // const RuntimeShape& bias_shape, const int32_t* bias_data,
-    dump_c_array("bias_shape", &bias_shape, sizeof(RuntimeShape));
-    dump_c_array("bias_data", bias_data, 4 * bias_shape.FlatSize());
-
-    // const RuntimeShape& output_shape, int8_t* output_data
-    dump_c_array("output_shape", &output_shape, sizeof(RuntimeShape));
-    dump_c_array("output_data", output_data, output_shape.FlatSize());
-    printf("\n\n");
-  }
-  invocation_count++;
-#endif
 
 // Uncomment to show min/max values for each
 #if 0
@@ -233,6 +203,37 @@ inline void ConvPerChannel(
       }
     }
   }
+#ifdef DUMP_LAYER
+  static int invocation_count = 0;
+  if (invocation_count == DUMP_LAYER) {
+    printf("\n\n");
+    // const ConvParams& params,
+    dump_c_array("params", &params, sizeof(ConvParams));
+
+    // const int32_t* output_multiplier, const int32_t* output_shift,
+    dump_c_array("output_multiplier", output_multiplier, 4 * output_depth);
+    dump_c_array("output_shift", output_shift, 4 * output_depth);
+
+    // const RuntimeShape& input_shape, const int8_t* input_data,
+    dump_c_array("input_shape", &input_shape, sizeof(RuntimeShape));
+    dump_c_array("input_data", input_data, input_shape.FlatSize());
+
+    // const RuntimeShape& filter_shape, const int8_t* filter_data,
+    dump_c_array("filter_shape", &filter_shape, sizeof(RuntimeShape));
+    dump_c_array("filter_data", filter_data, filter_shape.FlatSize());
+
+    // const RuntimeShape& bias_shape, const int32_t* bias_data,
+    dump_c_array("bias_shape", &bias_shape, sizeof(RuntimeShape));
+    dump_c_array("bias_data", bias_data, 4 * bias_shape.FlatSize());
+
+    // const RuntimeShape& output_shape, int8_t* output_data
+    dump_c_array("output_shape", &output_shape, sizeof(RuntimeShape));
+    dump_c_array("output_data", output_data, output_shape.FlatSize());
+    printf("\n\n");
+  }
+  invocation_count++;
+#endif
+
 }
 
 // Fixed-point per-channel-quantization convolution reference kernel.
