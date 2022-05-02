b0b1e738552c966a59c9199248a91e4666df20e9
Alan Green <alan.green@gmail.com>
2022-05-02 13:44:09 +1000

    fccum_tutorial/proj_menu: new test case
    Adds a test which, on failure, shows expected result and actual result
    together.
    
    Signed-off-by: Alan Green <avg@google.com>

--- proj/fccm_tutorial/src/proj_menu.cc
+++ proj/fccm_tutorial/src/proj_menu.cc
@@ -26,17 +26,28 @@ namespace {
 // Template Fn
 void do_hello_world(void) { puts("Hello, World!!!\n"); }
 
+void check_cfu(uint32_t a, uint32_t b, uint32_t expected) {
+  cfu_accumulate(a, b);
+  uint32_t actual = cfu_read();
+  printf("%08lx ", actual);
+  if (actual == expected) {
+    printf("ok\n");
+  } else {
+    printf("FAIL (0x%08lx) %ld != %ld\n", expected, actual, expected);
+  }
+}
+
 // Tests multiply-add CFU
 void do_test_cfu(void) {
   printf("\r\nCFU Test... ");
 
-  // Calculated on a spreadsheet
   cfu_reset();
-  cfu_accumulate(0x884CF61A, 0xE49F2F1C);
-  cfu_accumulate(0x0BE31854, 0x527FDBCF);
-  cfu_accumulate(0xADB43251, 0x4E36D172);
-  cfu_accumulate(0x6F867FFB, 0x442C7B76);
-  printf("%s\n", static_cast<int32_t>(cfu_read()) == 81978 ? "PASS!" : "FAIL");
+  printf("%08lx\n", cfu_read());
+
+  check_cfu(0x00808080, 0x98000000, -13312);
+  check_cfu(0x80B38080, 0x002B0000, -11119);
+  check_cfu(0x8080E180, 0x0000AE00, -19073);
+  check_cfu(0x8080801C, 0x000000AD, -32021);
 }
 
 struct Menu MENU = {
