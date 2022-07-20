5c270381ef0b5efdf30b50abc6c3990aaa96a571
Dan Callaghan <dcallagh@google.com>
2022-07-20 16:30:31 +1000

    models/hps: add latest "shared" model
    Taken from:
    https://chromium.googlesource.com/chromiumos/platform/hps-firmware/+/ec8183f8e3c117a91e23dccbd463ba0e848ae70d
    
    Unlike the earlier HPS models, this model produces two scores, which
    necessitates some unfortunate contortions in the golden test code.
    
    Also load the latest model by default, since it's the most important one
    to run golden tests against.
    
    Signed-off-by: Dan Callaghan <dcallagh@google.com>

--- common/src/models/hps_model/hps_model.cc
+++ common/src/models/hps_model/hps_model.cc
@@ -29,16 +29,36 @@
 #include "models/hps_model/presence_0_320_240_1_20220415_172145_201k_26k_96ops.h"
 #include "models/hps_model/second_0_320_240_1_20220117_135512_201k_26k_96ops.h"
 #include "models/hps_model/second_0_320_240_1_20220416_163802_201k_26k_96ops.h"
+#include "models/hps_model/shared_20220623_1032_tiled.h"
 #include "tflite.h"
 namespace {
 
 int loaded_model = 0;
+int num_outputs = 1;
+
+struct ModelResult {
+  int8_t score1;
+  int8_t score2;  // not produced by all models
+};
+
+void print_result(const ModelResult& result) {
+  switch (num_outputs) {
+    case 2:
+      printf("Result is (%d, %d)\n", result.score1, result.score2);
+      break;
+    case 1:
+    default:
+      printf("Result is (%d)\n", result.score1);
+      break;
+  }
+}
 
 // Initialize model
 void do_init_09_20(void) {
   puts("Loading HPS 09_20 model");
   tflite_load_model(hps_model_2021_09_20_tiled, hps_model_2021_09_20_tiled_len);
   loaded_model = 0;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -46,6 +66,7 @@ void do_init_01_05_74ops(void) {
   puts("Loading HPS 01_05_74ops model");
   tflite_load_model(hps_model_2022_01_05_74ops, hps_model_2022_01_05_74ops_len);
   loaded_model = 1;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -53,6 +74,7 @@ void do_init_01_05_89ops(void) {
   puts("Loading HPS 01_05_89ops model");
   tflite_load_model(hps_model_2022_01_05_89ops, hps_model_2022_01_05_89ops_len);
   loaded_model = 2;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -61,6 +83,7 @@ void do_init_presence_2022017_96ops(void) {
   tflite_load_model(presence_0_320_240_1_20220117_140437_201k_26k_96ops,
                     presence_0_320_240_1_20220117_140437_201k_26k_96ops_len);
   loaded_model = 3;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -69,6 +92,7 @@ void do_init_second_2022017_96ops(void) {
   tflite_load_model(second_0_320_240_1_20220117_135512_201k_26k_96ops,
                     second_0_320_240_1_20220117_135512_201k_26k_96ops_len);
   loaded_model = 4;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -77,6 +101,7 @@ void do_init_presence_20220415_96ops(void) {
   tflite_load_model(presence_0_320_240_1_20220415_172145_201k_26k_96ops,
                     presence_0_320_240_1_20220415_172145_201k_26k_96ops_len);
   loaded_model = 5;
+  num_outputs = 1;
 }
 
 // Initialize model
@@ -85,75 +110,145 @@ void do_init_second_20220416_96ops(void) {
   tflite_load_model(second_0_320_240_1_20220416_163802_201k_26k_96ops,
                     second_0_320_240_1_20220416_163802_201k_26k_96ops_len);
   loaded_model = 6;
+  num_outputs = 1;
 }
+
+// Initialize model
+void do_init_shared_20220623_1032_tiled(void) {
+  puts("Loading shared 20220623 model");
+  tflite_load_model(shared_20220623_1032_tiled, shared_20220623_1032_tiled_len);
+  loaded_model = 7;
+  num_outputs = 2;
+}
+
 // Run classification and interpret results
-int32_t classify() {
+ModelResult classify() {
   tflite_classify();
   int8_t* output = tflite_get_output();
-  return output[0];
+  switch (num_outputs) {
+    case 2:
+      return ModelResult{output[0], output[1]};
+    case 1:
+    default:
+      return ModelResult{output[0], 0};
+  }
 }
 
 // Classify with random data
-int32_t classify_cat() {
+ModelResult classify_cat() {
   tflite_set_input_unsigned(cat_picture);
   return classify();
 }
 
-void do_classify_cat() { printf("Result is %ld\n", classify_cat()); }
+void do_classify_cat() { print_result(classify_cat()); }
 
 // Classify with a grid pattern
-int32_t classify_diagram() {
+ModelResult classify_diagram() {
   tflite_set_input_unsigned(diagram);
   return classify();
 }
 
-void do_classify_diagram() { printf("Result is %ld\n", classify_diagram()); }
+void do_classify_diagram() { print_result(classify_diagram()); }
 
 // Set input to zero and run classification
-int32_t classify_zeros() {
+ModelResult classify_zeros() {
   tflite_set_input_zeros();
   return classify();
 }
 
-void do_classify_zeros() { printf("Result is %ld\n", classify_zeros()); }
+void do_classify_zeros() { print_result(classify_zeros()); }
 
 // Classify hps_* inputs
 
-int32_t classify_hps_0() {
+ModelResult classify_hps_0() {
   tflite_set_input_unsigned(hps_0);
   return classify();
 }
 
-void do_classify_hps_0() { printf("Result is %ld\n", classify_hps_0()); }
+void do_classify_hps_0() { print_result(classify_hps_0()); }
 
-int32_t classify_hps_1() {
+ModelResult classify_hps_1() {
   tflite_set_input_unsigned(hps_1);
   return classify();
 }
 
-void do_classify_hps_1() { printf("Result is %ld\n", classify_hps_1()); }
+void do_classify_hps_1() { print_result(classify_hps_1()); }
 
-int32_t classify_hps_2() {
+ModelResult classify_hps_2() {
   tflite_set_input_unsigned(hps_2);
   return classify();
 }
 
-void do_classify_hps_2() { printf("Result is %ld\n", classify_hps_2()); }
+void do_classify_hps_2() { print_result(classify_hps_2()); }
 
 // Golden tests: expected results
 struct GoldenTest {
-  int32_t (*fn)();
+  ModelResult (*fn)();
   const char* name;
-  int32_t expected[7];
+  ModelResult expected[8];
 };
 
 GoldenTest golden_tests[] = {
-    {classify_cat, "cat", {-77, -47, -47, -101, -117, -100, -108}},
-    {classify_diagram, "diagram", {-124, -123, -123, -124, -127, -117, -127}},
-    {classify_zeros, "zeroes", {-126, -128, -128, -128, -128, -109, -126}},
-    {classify_hps_0, "hps_0", {-119, -75, -75, -73, -125, -115, -128}},
-    {classify_hps_1, "hps_1", {124, 127, 127, 121, -119, 127, -128}},
-    {classify_hps_2, "hps_2", {126, 127, 127, 125, 127, 127, 127}},
+    {classify_cat,
+     "cat",
+     {{-77, 0},
+      {-47, 0},
+      {-47, 0},
+      {-101, 0},
+      {-117, 0},
+      {-100, 0},
+      {-108, 0},
+      {-112, -125}}},
+    {classify_diagram,
+     "diagram",
+     {{-124, 0},
+      {-123, 0},
+      {-123, 0},
+      {-124, 0},
+      {-127, 0},
+      {-117, 0},
+      {-127, 0},
+      {-128, -128}}},
+    {classify_zeros,
+     "zeroes",
+     {{-126, 0},
+      {-128, 0},
+      {-128, 0},
+      {-128, 0},
+      {-128, 0},
+      {-109, 0},
+      {-126, 0},
+      {-119, -124}}},
+    {classify_hps_0,
+     "hps_0",
+     {{-119, 0},
+      {-75, 0},
+      {-75, 0},
+      {-73, 0},
+      {-125, 0},
+      {-115, 0},
+      {-128, 0},
+      {-91, -128}}},
+    {classify_hps_1,
+     "hps_1",
+     {{124, 0},
+      {127, 0},
+      {127, 0},
+      {121, 0},
+      {-119, 0},
+      {127, 0},
+      {-128, 0},
+      {127, -128}}},
+    {classify_hps_2,
+     "hps_2",
+     {{126, 0},
+      {127, 0},
+      {127, 0},
+      {125, 0},
+      {127, 0},
+      {127, 0},
+      {127, 0},
+      {127, 127}}},
     {nullptr, "", 0},
 };
 
@@ -162,11 +257,16 @@ static void do_golden_tests() {
   for (size_t i = 0; golden_tests[i].fn; i++) {
     const GoldenTest& test = golden_tests[i];
     printf("Testing input %s: ", test.name);
-    int32_t actual = test.fn();
-    int32_t expected = test.expected[loaded_model];
-    if (actual != expected) {
+    ModelResult actual = test.fn();
+    ModelResult expected = test.expected[loaded_model];
+    if (actual.score1 != expected.score1) {
       failed = true;
-      printf("FAIL %ld (actual) != %ld (expected) ***\n", actual, expected);
+      printf("FAIL score1 %d (actual) != %d (expected) ***\n", actual.score1,
+             expected.score1);
+    } else if (actual.score2 != expected.score2) {
+      failed = true;
+      printf("FAIL score2 %d (actual) != %d (expected) ***\n", actual.score2,
+             expected.score2);
     } else {
       printf("OK\n");
     }
@@ -204,6 +304,8 @@ struct Menu MENU = {
                   do_init_presence_20220415_96ops),
         MENU_ITEM('6', "Reinitialize with second_20220416_96ops model",
                   do_init_second_20220416_96ops),
+        MENU_ITEM('7', "Reinitialize with shared_20220623_1032_tiled model",
+                  do_init_shared_20220623_1032_tiled),
         MENU_END,
     },
 };
@@ -212,6 +314,6 @@ struct Menu MENU = {
 
 // For integration into menu system
 extern "C" void hps_model_menu() {
-  do_init_01_05_74ops();
+  do_init_shared_20220623_1032_tiled();
   menu_run(&MENU);
 }
