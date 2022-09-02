ffa13b40319a96674372f1477df843e3b4e3c2c4
Shvetank Prakash <46943363+ShvetankPrakash@users.noreply.github.com>
2022-09-02 14:36:09 -0400

    --no-ident-version LITEX ARG should only be used for bitstream build

--- proj/dse_template/dse_framework.py
+++ proj/dse_template/dse_framework.py
@@ -86,10 +86,11 @@ def run_config(variant, target):
     cells  = None
     run_succeeds = False 
     while not run_succeeds:
-        EXTRA_LITEX_ARGS = 'EXTRA_LITEX_ARGS="--no-ident-version --cpu-variant=' + variant +'"'
+        BITSTREAM_EXTRA_LITEX_ARGS = 'EXTRA_LITEX_ARGS="--no-ident-version --cpu-variant=' + variant +'"'
+        SIM_EXTRA_LITEX_ARGS = 'EXTRA_LITEX_ARGS="--cpu-variant=' + variant +'"'
         subprocess.run(['make', 'clean']) 
-        subprocess.run(['make', 'bitstream', 'TARGET=' + target, EXTRA_LITEX_ARGS, "USE_SYMBIFLOW=1"])
-        workload_cmd = ['make', 'load', 'PLATFORM=sim', EXTRA_LITEX_ARGS]
+        subprocess.run(['make', 'bitstream', 'TARGET=' + target, BITSTREAM_EXTRA_LITEX_ARGS, "USE_SYMBIFLOW=1"])
+        workload_cmd = ['make', 'load', 'PLATFORM=sim', SIM_EXTRA_LITEX_ARGS]
         filename = 'cycle_count.rpt'
         outfile  = open(filename, "w")
         workload = subprocess.run(workload_cmd, stdout=outfile)
