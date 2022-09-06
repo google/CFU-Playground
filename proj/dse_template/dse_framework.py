3c3a8fb5ab1aa0fd45c8adcc49950efb67d2ef90
Shvetank Prakash <sprakash@g.harvard.edu>
2022-09-06 16:56:53 -0400

    Update invocation of bitstream build using --no-ident-version arg

--- proj/dse_template/dse_framework.py
+++ proj/dse_template/dse_framework.py
@@ -89,7 +89,8 @@ def run_config(variant, target):
         BITSTREAM_EXTRA_LITEX_ARGS = 'EXTRA_LITEX_ARGS="--no-ident-version --cpu-variant=' + variant +'"'
         SIM_EXTRA_LITEX_ARGS = 'EXTRA_LITEX_ARGS="--cpu-variant=' + variant +'"'
         subprocess.run(['make', 'clean']) 
-        subprocess.run(['make', 'bitstream', 'TARGET=' + target, BITSTREAM_EXTRA_LITEX_ARGS, "USE_SYMBIFLOW=1"])
+        os.system("make bitstream TARGET=" + target + " " + BITSTREAM_EXTRA_LITEX_ARGS + " USE_SYMBIFLOW=1")
+        #subprocess.run(['make', 'bitstream', 'TARGET=' + target, BITSTREAM_EXTRA_LITEX_ARGS, "USE_SYMBIFLOW=1"])
         workload_cmd = ['make', 'load', 'PLATFORM=sim', SIM_EXTRA_LITEX_ARGS]
         filename = 'cycle_count.rpt'
         outfile  = open(filename, "w")
