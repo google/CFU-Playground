0468d17dbaf2883da0b0a534e2c03c48d4080b3d
Pavlo Hilei <p.hilei@gmail.com>
2023-04-01 00:14:53 +0200

    UPD

---
+++ proj/simc_3_MIXED_v2_software/cfu_v5.v
@@ -0,0 +1,317 @@
+/*
+    cmd: 
+        - 0 - initialize - Fills paddings with 0
+        - 1 - write to input buffer
+            - inp0 - address / 4
+            - inp1 - 4 values
+        - 2 - write to kernel weights buffer
+            - inp0 - address / 4
+            - inp1 - 4 values
+        - 3 - read from output_buffer 
+            - inp0 - address / 4
+        - 4 - start computation
+        
+        // debug purposes
+        - 5 - read from the input buffer
+            - inp0 - address / 4
+        - 6 - read from the kernel weights
+            - inp0 - addres / 4
+
+        // 
+        - 7 - set bias
+            - inp0 - bias
+        - 8 - set input offset
+            - inp0 - input_offset
+        
+*/
+module conv1d #(
+    parameter BYTE_SIZE = 8,
+    parameter INT32_SIZE = 32
+)(
+    input                         clk,
+    input       [6:0]             cmd,
+    input       [INT32_SIZE-1:0]  inp0,
+    input       [INT32_SIZE-1:0]  inp1,
+    output reg  [INT32_SIZE-1:0]  ret,
+    output reg                    output_buffer_valid = 1
+);
+localparam PADDING = 4; // (8 / 2)
+localparam MAX_INPUT_SIZE = 1024;
+localparam MAX_INPUT_CHANNELS = 128;
+localparam KERNEL_LENGTH = 8;
+
+
+wire [INT32_SIZE-1:0] address = inp0;
+wire [INT32_SIZE-1:0] value   = inp1;
+
+// INPUT/OUTPUT BUFFER
+reg [INT32_SIZE-1:0]       buffer_size;
+// Input buffer has shape of 1024 x 128 
+reg signed [BYTE_SIZE-1:0] input_buffer          [0:MAX_INPUT_SIZE * MAX_INPUT_CHANNELS - 1];
+// kernel weights buffer has shape of 8 x 128 
+reg signed [BYTE_SIZE-1:0] kernel_weights_buffer [0:KERNEL_LENGTH * MAX_INPUT_CHANNELS - 1];
+// output buffer has a shape of 1024
+reg signed [INT32_SIZE:0]  output_buffer         [0:MAX_INPUT_SIZE - 1];
+
+reg signed [INT32_SIZE-1:0] input_offset = 32'd0;
+reg signed [INT32_SIZE-1:0] output_offset = 32'd0;
+reg signed [INT32_SIZE-1:0] output_activation_min = 32'd0;
+reg signed [INT32_SIZE-1:0] output_activation_max = 32'd0;
+reg signed [INT32_SIZE-1:0] output_depth = 32'd0;
+reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
+reg signed [INT32_SIZE-1:0] input_depth = 32'd0;
+
+reg signed [INT32_SIZE-1:0] bias = 32'd0;
+reg signed [INT32_SIZE-1:0] output_multiplier = 32'd0;
+reg signed [INT32_SIZE-1:0] output_shift = 32'd0;
+
+reg signed [BYTE_SIZE-1:0] input_val = 32'd0;
+reg signed [BYTE_SIZE-1:0] filter_val = 32'd0;
+wire signed [INT32_SIZE-1:0] test_val = filter_val * (input_val + input_offset);
+
+reg signed [INT32_SIZE-1:0] acc = 0;
+reg signed [INT32_SIZE-1:0] in_x_origin = 0;
+
+always @(posedge clk) begin
+    // Note that processor is little-endian
+    case (cmd)
+        // Initialize
+        0: begin    // Reset module
+            // Fill output with zeros
+            for (reg [INT32_SIZE-1:0] out_x = 0; out_x < MAX_INPUT_SIZE; out_x = out_x + 1) begin
+                output_buffer[out_x] = 0;
+            end 
+
+            // Fill input with zeros
+            for (reg [INT32_SIZE-1:0] in_idx = 0; in_idx < MAX_INPUT_SIZE * MAX_INPUT_CHANNELS; in_idx = in_idx + 1) begin
+                input_buffer[in_idx] = 0;
+            end 
+
+            // Fill kernel_weights_buffer with zeros
+            for (reg [INT32_SIZE-1:0] kernel_idx = 0; kernel_idx < KERNEL_LENGTH * MAX_INPUT_CHANNELS; kernel_idx = kernel_idx + 1) begin
+                kernel_weights_buffer[kernel_idx] = 0;
+            end   
+        end 
+        1: begin
+            input_val <= value;
+        end
+        2: begin
+            filter_val <= value;
+        end
+        3: begin
+            ret <= test_val;
+        end
+        4: begin
+            ret[7:0] <= input_val;
+        end
+        5: begin
+            ret[7:0] <= filter_val;
+        end
+
+        // Write buffers
+        10: begin    // Write input buffer
+            input_buffer[address] <= value[7:0];
+        end
+        11: begin    // Write kernel weights buffer
+            // Note that processor is little-endian
+            kernel_weights_buffer[address] <= value[7:0];
+        end
+
+        // Read buffers
+        12: begin    // Read output buffer
+            // Note that processor is little-endian
+            ret <= output_buffer[address];
+        end
+        13: begin
+            ret[7 : 0] <= input_buffer[address];
+        end
+        14: begin
+            ret[7 : 0] <= kernel_weights_buffer[address];
+        end
+        15: begin    // Zero out output buffer
+            for (reg [INT32_SIZE-1:0] out_x = 0; out_x < MAX_INPUT_SIZE; out_x = out_x + 1) begin
+                output_buffer[out_x] = 0;
+            end 
+        end
+
+        // Write parameters
+        20: begin
+            input_offset <= value;
+        end
+
+        21: begin
+            output_offset <= value;
+        end
+
+        22: begin
+            output_activation_min <= value;
+        end
+        23: begin
+            output_activation_max <= value;
+        end
+        24: begin
+            output_depth <= value;
+        end
+        25: begin
+            input_output_width <= value;
+        end
+        26: begin
+            input_depth <= value;
+        end
+        27: begin
+            bias <= value;
+        end
+        28: begin
+            output_multiplier <= value;
+        end
+        29: begin
+            output_shift <= value;
+        end
+
+        40: begin    // Start computation
+            // output_buffer_valid = 0;
+            for (reg signed [INT32_SIZE-1:0] out_x = 0; out_x < MAX_INPUT_SIZE; out_x = out_x + 1) begin
+                reg signed [INT32_SIZE-1:0] in_x_origin = out_x - 3;
+                for (reg signed [INT32_SIZE-1:0] filter_x = 0; filter_x < 8; filter_x = filter_x + 1) begin
+                    reg signed [INT32_SIZE-1:0] in_x = in_x_origin + filter_x;
+                    for (reg signed [INT32_SIZE-1:0] in_channel = 0; in_channel < MAX_INPUT_CHANNELS; in_channel = in_channel + 1) begin
+                        if ((in_x >= 0) && (in_x < input_output_width) && (in_channel < input_depth)) begin
+                            output_buffer[out_x] += kernel_weights_buffer[filter_x * input_depth + in_channel]
+                                    * (input_buffer[in_x * input_depth + in_channel] + input_offset);
+                        end
+                    end
+                end
+                output_buffer[out_x] += bias; 
+            end
+            // output_buffer_valid = 1;
+        end
+
+        41: begin 
+            acc = 0;
+            for (reg signed [INT32_SIZE-1:0] filter_x = 0; filter_x < 8; filter_x = filter_x + 1) begin
+                reg signed [INT32_SIZE-1:0] in_x = in_x_origin + filter_x;
+                for (reg signed [INT32_SIZE-1:0] in_channel = 0; in_channel < MAX_INPUT_CHANNELS; in_channel = in_channel + 1) begin
+                    if ((in_x >= 0) && (in_x < input_output_width) && (in_channel < input_depth)) begin
+                        acc += kernel_weights_buffer[filter_x * input_depth + in_channel] 
+                            * (input_buffer[in_x * input_depth + in_channel] + input_offset);
+                    end
+                end
+            end
+        end
+        42: begin
+            in_x_origin <= value;
+        end
+        43: begin
+            ret <= acc;
+        end
+    endcase
+end
+    
+endmodule
+
+module Cfu (
+  input               cmd_valid,
+  output              cmd_ready,
+  input      [9:0]    cmd_payload_function_id,
+  input      [31:0]   cmd_payload_inputs_0,
+  input      [31:0]   cmd_payload_inputs_1,
+  output reg          rsp_valid,
+  input               rsp_ready,
+  output wire [31:0]  rsp_payload_outputs_0,
+  input               reset,
+  input               clk
+);
+  wire output_buffer_valid;
+  
+  wire [6:0] funct7;
+  assign funct7 = cmd_payload_function_id[9:3];
+  wire [31:0] ret;
+
+  conv1d CONV_1D(
+    .clk(clk),
+    .cmd(funct7),
+    .inp0(cmd_payload_inputs_0),
+    .inp1(cmd_payload_inputs_1),
+    .ret(rsp_payload_outputs_0),
+    .output_buffer_valid(output_buffer_valid)
+  );
+
+  // Only not ready for a command when we have a response.
+  assign cmd_ready = ~rsp_valid;
+
+  always @(posedge clk) begin
+    if (reset) begin
+      rsp_valid <= 1'b0;
+    end else if (rsp_valid) begin
+      // Waiting to hand off response to CPU.
+      rsp_valid <= ~rsp_ready;
+    end else if (cmd_valid) begin
+        rsp_valid <= output_buffer_valid;
+    end
+  end
+endmodule
+
+
+
+
+
+// module Cfu (
+//   input               cmd_valid,
+//   output              cmd_ready,
+//   input      [9:0]    cmd_payload_function_id,
+//   input      [31:0]   cmd_payload_inputs_0,
+//   input      [31:0]   cmd_payload_inputs_1,
+//   output reg          rsp_valid,
+//   input               rsp_ready,
+//   output reg [31:0]   rsp_payload_outputs_0,
+//   input               reset,
+//   input               clk
+// );
+//   // localparam InputOffset = $signed(9'd128);
+//   reg signed [8:0] input_offset = 128;
+  
+//   wire [6:0] funct7;
+//   assign funct7 = cmd_payload_function_id[9:3];
+
+//   // SIMD multiply step:
+//   wire signed [15:0] prod_0, prod_1, prod_2, prod_3;
+//   assign prod_0 =  ($signed(cmd_payload_inputs_0[7 : 0]) + input_offset)
+//                   * $signed(cmd_payload_inputs_1[7 : 0]);
+//   assign prod_1 =  ($signed(cmd_payload_inputs_0[15: 8]) + input_offset)
+//                   * $signed(cmd_payload_inputs_1[15: 8]);
+//   assign prod_2 =  ($signed(cmd_payload_inputs_0[23:16]) + input_offset)
+//                   * $signed(cmd_payload_inputs_1[23:16]);
+//   assign prod_3 =  ($signed(cmd_payload_inputs_0[31:24]) + input_offset)
+//                   * $signed(cmd_payload_inputs_1[31:24]);
+
+//   wire signed [31:0] sum_prods;
+//   assign sum_prods = prod_0 + prod_1 + prod_2 + prod_3;
+
+//   // Only not ready for a command when we have a response.
+//   assign cmd_ready = ~rsp_valid;
+
+//   always @(posedge clk) begin
+//     if (reset) begin
+//       rsp_payload_outputs_0 <= 32'b0;
+//       rsp_valid <= 1'b0;
+//     end else if (rsp_valid) begin
+//       // Waiting to hand off response to CPU.
+//       rsp_valid <= ~rsp_ready;
+//     end else if (cmd_valid) begin
+//       rsp_valid <= 1'b1;
+//       // Accumulate step:
+//       if (funct7 == 0) begin
+//         rsp_payload_outputs_0 <= rsp_payload_outputs_0 + sum_prods;
+//       end else if (funct7 == 1) begin
+//         rsp_payload_outputs_0 <= 32'b0;
+//       end else if (funct7 == 2) begin
+//         input_offset <= cmd_payload_inputs_0;
+//       end else begin
+//         rsp_payload_outputs_0 <= input_offset;
+//       end
+//       // rsp_payload_outputs_0 <= |cmd_payload_function_id[9:3]
+//       //     ? 32'b0
+//       //     : rsp_payload_outputs_0 + sum_prods;
+//     end
+//   end
+// endmodule
--- proj/simc_3_MIXED_v2_software/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/simc_3_MIXED_v2_software/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -40,13 +40,15 @@ limitations under the License.
 namespace tflite {
 namespace reference_integer_ops {
 
+// #define ConvPerChannel ConvPerChannelVeryOriginal
 // #define ConvPerChannel ConvPerChannelOriginal
 // #define ConvPerChannel ConvPerChannelCFU
 // #define ConvPerChannel ConvPerChannelCFUSoftware1
 // #define ConvPerChannel ConvPerChannelCFUSoftware2
 // #define ConvPerChannel ConvPerChannelCFUSoftware3
 // #define ConvPerChannel ConvPerChannelCFUSoftware4
-#define ConvPerChannel ConvPerChannelCFUHardware5
+// #define ConvPerChannel ConvPerChannelCFUHardware5
+#define ConvPerChannel ConvPerChannelCFUHardware6
 // #define ConvPerChannel ConvPerChannelOriginalSimple
 #define DEBUG_PRINTS 1
 
@@ -81,6 +83,89 @@ inline int32_t multiply_by_quant_mult(int32_t x, int32_t quantized_multiplier, i
       right_shift);
 }
 }  // namespace
+
+inline void ConvPerChannelCFUHardware6(const ConvParams& params,
+                                       const int32_t* output_multiplier,
+                                       const int32_t* output_shift,
+                                       const RuntimeShape& input_shape,
+                                       const int8_t* input_data,
+                                       const RuntimeShape& filter_shape,
+                                       const int8_t* filter_data,
+                                       const RuntimeShape& bias_shape,
+                                       const int32_t* bias_data,
+                                       const RuntimeShape& output_shape,
+                                       int8_t* output_data) {
+  // static int call = 0;
+  // Initialize cfu
+  cfu_op0(0, 0, 0);
+
+  // Get parameters.
+  const int32_t input_offset = params.input_offset;  // r = s(q - Z)
+  cfu_op0(20, 0, input_offset);
+
+  const int pad_width = 3;
+  (void)pad_width;
+
+  const int32_t output_offset = -128;
+  cfu_op0(21, 0, output_offset);
+  const int32_t output_activation_min = -128;
+  cfu_op0(22, 0, output_activation_min);
+  const int32_t output_activation_max = 127;
+  cfu_op0(23, 0, output_activation_max);
+
+  const int output_depth = MatchingDim(filter_shape, 0, output_shape, 3);
+  cfu_op0(24, 0, output_depth);
+
+  const int input_width = input_shape.Dims(2);
+  cfu_op0(25, 0, input_width);
+
+  const int filter_width = 8;
+  (void)filter_width;
+
+  const int input_depth = filter_shape.Dims(3);
+  cfu_op0(26, 0, input_depth);
+
+  // const int output_width = output_shape.Dims(2);
+  const int output_width = input_width;
+
+  // Copy input
+  for (int in_x = 0; in_x < input_width; ++in_x) {
+    for (int in_channel = 0; in_channel < input_depth; ++in_channel) {
+      int addr = in_x * input_depth + in_channel;
+      cfu_op0(10, addr, input_data[addr]);
+    }
+  }
+
+  for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
+    // Copy kernel
+    for (int in_channel = 0; in_channel < input_depth; ++in_channel) {
+      for (int kernel_x = 0; kernel_x < filter_width; ++kernel_x) {
+        int addr = out_channel * (8 * input_depth) + kernel_x * input_depth + in_channel;
+        cfu_op0(11, kernel_x * input_depth + in_channel, filter_data[addr]);
+      }
+    }
+    cfu_op0(27, 0, bias_data[out_channel]);
+
+    for (int out_x = 0; out_x < output_width; ++out_x) {
+      const int in_x_origin = out_x - pad_width;
+
+      cfu_op0(42, 0, in_x_origin);
+      cfu_op0(41, 0, 0);
+      int32_t acc = cfu_op0(43, 0, 0);
+
+      if (bias_data) {
+        acc += bias_data[out_channel];
+      }
+      acc = multiply_by_quant_mult(acc, output_multiplier[out_channel], output_shift[out_channel]);
+
+      acc += output_offset;
+      acc               = std::max(acc, output_activation_min);
+      acc               = std::min(acc, output_activation_max);
+      int addr          = out_x * output_depth + out_channel;
+      output_data[addr] = static_cast<int8_t>(acc);
+    }
+  }
+}
 
 inline void ConvPerChannelCFUHardware5(const ConvParams& params,
                                        const int32_t* output_multiplier,
@@ -164,47 +249,6 @@ inline void ConvPerChannelCFUHardware5(const ConvParams& params,
       output_data[addr] = static_cast<int8_t>(acc);
     }
   }
-
-  // // Copy input
-  // for (int in_x = 0; in_x < input_width; ++in_x) {
-  //   for (int in_channel = 0; in_channel < input_depth; ++in_channel) {
-  //     int addr = in_x * input_depth + in_channel;
-  //     cfu_op0(10, addr, input_data[addr]);
-  //   }
-  // }
-
-  // for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
-  //   // Copy output_multiplier, output_shift, bias
-  //   cfu_op0(28, 0, output_multiplier[out_channel]);
-  //   cfu_op0(29, 0, output_shift[out_channel]);
-  //   cfu_op0(27, 0, bias_data[out_channel]);
-
-  //   // Copy kernel
-  //   for (int in_channel = 0; in_channel < input_depth; ++in_channel) {
-  //     for (int kernel_x = 0; kernel_x < filter_width; ++kernel_x) {
-  //       int addr = out_channel * (8 * input_depth) + kernel_x * input_depth + in_channel;
-  //       cfu_op0(11, kernel_x * input_depth + in_channel, filter_data[addr]);
-  //     }
-  //   }
-
-  //   // Start Convolution
-  //   cfu_op0(40, 0, 0);
-
-  //   for (int out_x = 0; out_x < output_width; ++out_x) {
-  //     int addr    = out_x * output_depth + out_channel;
-  //     int32_t acc = static_cast<int32_t>(cfu_op0(12, out_x, 0));
-  //     acc = multiply_by_quant_mult(acc, output_multiplier[out_channel],
-  //     output_shift[out_channel]);
-
-  //     acc += output_offset;
-  //     acc = std::max(acc, output_activation_min);
-  //     acc = std::min(acc, output_activation_max);
-
-  //     output_data[addr] = acc;
-  //   }
-  //   // Zero out output buffer
-  //   cfu_op0(15, 0, 0);
-  // }
 }
 
 inline void ConvPerChannelCFUSoftware4(const ConvParams& params,
@@ -1063,6 +1107,118 @@ inline void ConvPerChannelCFU(const ConvParams& params,
 
   // abort();
 }
+
+inline void ConvPerChannelVeryOriginal(
+    const ConvParams& params, const int32_t* output_multiplier,
+    const int32_t* output_shift, const RuntimeShape& input_shape,
+    const int8_t* input_data, const RuntimeShape& filter_shape,
+    const int8_t* filter_data, const RuntimeShape& bias_shape,
+    const int32_t* bias_data, const RuntimeShape& output_shape,
+    int8_t* output_data) {
+  // Get parameters.
+  const int32_t input_offset = params.input_offset;  // r = s(q - Z)
+  const int stride_width = params.stride_width;
+  const int stride_height = params.stride_height;
+  const int dilation_width_factor = params.dilation_width_factor;
+  const int dilation_height_factor = params.dilation_height_factor;
+  const int pad_width = params.padding_values.width;
+  const int pad_height = params.padding_values.height;
+  const int32_t output_offset = params.output_offset;
+
+  // Set min and max value of the output.
+  const int32_t output_activation_min = params.quantized_activation_min;
+  const int32_t output_activation_max = params.quantized_activation_max;
+
+  // Consistency check.
+  TFLITE_DCHECK_LE(output_activation_min, output_activation_max);
+  TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(filter_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(output_shape.DimensionsCount(), 4);
+  const int batches = MatchingDim(input_shape, 0, output_shape, 0);
+  const int input_depth = input_shape.Dims(3);
+  const int output_depth = MatchingDim(filter_shape, 0, output_shape, 3);
+  if (bias_data) {
+    TFLITE_DCHECK_EQ(bias_shape.FlatSize(), output_depth);
+  }
+
+  // Check dimensions of the tensors.
+  const int input_height = input_shape.Dims(1);
+  const int input_width = input_shape.Dims(2);
+  const int filter_height = filter_shape.Dims(1);
+  const int filter_width = filter_shape.Dims(2);
+  const int filter_input_depth = filter_shape.Dims(3);
+  const int groups = input_depth / filter_input_depth;
+  TFLITE_DCHECK_EQ(input_depth % filter_input_depth, 0);
+  const int filters_per_group = output_depth / groups;
+  const int output_height = output_shape.Dims(1);
+  const int output_width = output_shape.Dims(2);
+  for (int batch = 0; batch < batches; ++batch) {
+    for (int out_y = 0; out_y < output_height; ++out_y) {
+      const int in_y_origin = (out_y * stride_height) - pad_height;
+      for (int out_x = 0; out_x < output_width; ++out_x) {
+        const int in_x_origin = (out_x * stride_width) - pad_width;
+        for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
+          auto group = out_channel / filters_per_group;
+          int32_t acc = 0;
+          for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
+            const int in_y = in_y_origin + dilation_height_factor * filter_y;
+            for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
+              const int in_x = in_x_origin + dilation_width_factor * filter_x;
+
+              // Zero padding by omitting the areas outside the image.
+              const bool is_point_inside_image =
+                  (in_x >= 0) && (in_x < input_width) && (in_y >= 0) &&
+                  (in_y < input_height);
+
+              if (!is_point_inside_image) {
+                continue;
+              }
+
+              for (int in_channel = 0; in_channel < filter_input_depth;
+                   ++in_channel) {
+                int32_t input_val =
+                    input_data[Offset(input_shape, batch, in_y, in_x,
+                                      in_channel + group * filter_input_depth)];
+                int32_t filter_val = filter_data[Offset(
+                    filter_shape, out_channel, filter_y, filter_x, in_channel)];
+                // Accumulate with 32 bits accumulator.
+                // In the nudging process during model quantization, we force
+                // real value of 0.0 be represented by a quantized value. This
+                // guarantees that the input_offset is a int8_t, even though
+                // it is represented using int32_t. int32_t += int8_t *
+                // (int8_t - int8_t) so the highest value we can get from each
+                // accumulation is [-127, 127] * ([-128, 127] -
+                // [-128, 127]), which is [-32512, 32512]. log2(32512)
+                // = 14.98, which means we can accumulate at least 2^16
+                // multiplications without overflow. The accumulator is
+                // applied to a filter so the accumulation logic will hold as
+                // long as the filter size (filter_y * filter_x * in_channel)
+                // does not exceed 2^16, which is the case in all the models
+                // we have seen so far.
+                // TODO(b/174275578): Add a check to make sure the
+                // accumulator depth is smaller than 2^16.
+                acc += filter_val * (input_val + input_offset);
+              }
+            }
+          }
+
+          if (bias_data) {
+            acc += bias_data[out_channel];
+          }
+          acc = MultiplyByQuantizedMultiplier(
+              acc, output_multiplier[out_channel], output_shift[out_channel]);
+          acc += output_offset;
+          acc = std::max(acc, output_activation_min);
+          acc = std::min(acc, output_activation_max);
+          output_data[Offset(output_shape, batch, out_y, out_x, out_channel)] =
+              static_cast<int8_t>(acc);
+        }
+      }
+    }
+  }
+}
+
+
 
 inline void ConvPerChannelWithPackedInt4Weights(const ConvParams& params,
                                                 const int32_t* output_multiplier,
