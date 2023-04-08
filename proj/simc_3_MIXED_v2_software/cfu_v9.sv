122844e922ac08ae29b60e868cddfa42cf003843
Pavlo Hilei <pavlikhiley@gmail.com>
2023-04-08 14:14:11 +0200

    UPD

--- proj/simc_3_MIXED_v2_software/cfu_v9.sv
+++ proj/simc_3_MIXED_v2_software/cfu_v9.sv
@@ -1,125 +1,4 @@
 // `include "cfu_v8_clean.sv"
-// module conv1d #(
-//     parameter BYTE_SIZE  = 8,
-//     parameter INT32_SIZE = 32
-// ) (
-//     input                       clk,
-//     input      [           6:0] cmd,
-//     input      [INT32_SIZE-1:0] inp0,
-//     input      [INT32_SIZE-1:0] inp1,
-//     output reg [INT32_SIZE-1:0] ret,
-//     output reg                  output_buffer_valid = 1
-// );
-//   localparam PADDING = 4;  // (8 / 2)
-//   localparam MAX_INPUT_SIZE = 1024;
-//   localparam MAX_INPUT_CHANNELS = 128;
-//   localparam KERNEL_LENGTH = 8;
-
-//   localparam BUFFERS_SIZE = KERNEL_LENGTH * MAX_INPUT_CHANNELS;
-//   localparam SUM_AT_ONCE = 16;
-//   localparam CLOCKS_TODO_JOB = BUFFERS_SIZE / SUM_AT_ONCE;
-
-
-//   wire [INT32_SIZE-1:0] address = inp0;
-//   wire [INT32_SIZE-1:0] value = inp1;
-
-//   // Buffers
-//   (* RAM_STYLE="BLOCK" *)
-//   reg signed [BYTE_SIZE-1:0] input_buffer[0:BUFFERS_SIZE - 1];
-
-//   (* RAM_STYLE="BLOCK" *)
-//   reg signed [BYTE_SIZE-1:0] kernel_weights_buffer[0:BUFFERS_SIZE - 1];
-
-//   // Parameters
-//   reg signed [INT32_SIZE-1:0] input_offset = 32'd0;
-//   reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
-//   reg signed [INT32_SIZE-1:0] input_depth = 32'd0;
-
-//   // Computation related registers
-//   reg signed [INT32_SIZE-1:0] start_filter_x = 0;
-//   reg finished_work = 1'b1;
-//   reg unsigned [INT32_SIZE-1:0] addr_counter = 0; // KERNEL_LENGTH * MAX_INPUT_CHANNELS = 8 * 128 = 1024 == 2**10
-//   reg signed [INT32_SIZE-1:0] acc = 0;
-
-//   always @(posedge clk) begin
-//     if (!finished_work) begin
-//       if (addr_counter >= BUFFERS_SIZE) begin
-//         finished_work = 1;
-//       end else begin
-//         acc <= acc + 
-//           kernel_weights_buffer[addr_counter +  1] * (input_buffer[(addr_counter +  1 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  2] * (input_buffer[(addr_counter +  2 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  3] * (input_buffer[(addr_counter +  3 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  4] * (input_buffer[(addr_counter +  4 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  5] * (input_buffer[(addr_counter +  5 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  6] * (input_buffer[(addr_counter +  6 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  7] * (input_buffer[(addr_counter +  7 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter +  8] * (input_buffer[(addr_counter +  8 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-
-//           kernel_weights_buffer[addr_counter +  9] * (input_buffer[(addr_counter +  9 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 10] * (input_buffer[(addr_counter + 10 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 11] * (input_buffer[(addr_counter + 11 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 12] * (input_buffer[(addr_counter + 12 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 13] * (input_buffer[(addr_counter + 13 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 14] * (input_buffer[(addr_counter + 14 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 15] * (input_buffer[(addr_counter + 15 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset) + 
-//           kernel_weights_buffer[addr_counter + 16] * (input_buffer[(addr_counter + 16 + start_filter_x * input_depth) % (8 * input_depth)] + input_offset);
-
-//         addr_counter = addr_counter + SUM_AT_ONCE;
-//       end
-
-
-//     end
-
-//     case (cmd)
-//       // Initialize
-//       0: begin  // Reset module
-//         // Fill input with zeros
-//       end
-
-//       // Write buffers
-//       1: begin  // Write input buffer
-//         input_buffer[address] <= value[7:0];
-//       end
-//       2: begin  // Write kernel weights buffer
-//         kernel_weights_buffer[address] <= value[7:0];
-//       end
-
-//       // Write parameters
-//       3: begin
-//         input_offset <= value;
-//       end
-
-//       4: begin
-//         input_output_width <= value;
-//       end
-//       5: begin
-//         input_depth <= value;
-//       end
-
-//       6: begin  // Start computation
-//         acc <= 0;
-//         finished_work = 0;
-//         addr_counter  = 0;
-//       end
-
-//       7: begin  // get acumulator
-//         ret <= acc;
-//       end
-//       8: begin  // Write start x in input ring buffer 
-//         start_filter_x <= value;
-//       end
-//       9: begin  // Check if computation is done
-//         ret <= finished_work;
-//       end
-//       default: begin
-//         // $display("!!! DEFAULT case ");
-//         ret <= 0;
-//       end
-//     endcase
-//   end
-
-// endmodule
 
 module conv1d #(
     parameter BYTE_SIZE  = 8,
@@ -139,10 +18,6 @@ module conv1d #(
 
   localparam SUM_AT_ONCE = 8;
   localparam BUFFERS_SIZE = KERNEL_LENGTH * MAX_INPUT_CHANNELS;
-  
-  // localparam BUFFERS_SIZE = SUM_AT_ONCE;
-  // localparam BUFFERS_SIZE = 8;
-
 
   wire [INT32_SIZE-1:0] address = inp0;
   wire [INT32_SIZE-1:0] value = inp1;
@@ -157,41 +32,43 @@ module conv1d #(
 
   // Parameters
   reg signed [INT32_SIZE-1:0] input_offset = 32'd0;
-  reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
+  // reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
   reg signed [INT32_SIZE-1:0] input_depth = 32'd0;
 
   // Computation related registers
   reg signed [INT32_SIZE-1:0] start_filter_x = 0;
   reg finished_work = 1'b1;
+  reg update_address = 1'b0;
   reg [INT32_SIZE-1:0] kernel_addr;
   reg [INT32_SIZE-1:0] input_addr;
   reg signed [INT32_SIZE-1:0] acc;
 
   always @(posedge clk) begin
     if (!finished_work) begin
-      if (kernel_addr >= cur_buffer_size) begin
-        finished_work <= 1;
-      end else begin
-        acc <= acc + 
-          kernel_weights_buffer[kernel_addr     ] * (input_buffer[(input_addr    )] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  1] * (input_buffer[(input_addr + 1)] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  2] * (input_buffer[(input_addr + 2)] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  3] * (input_buffer[(input_addr + 3)] + input_offset) +
-          kernel_weights_buffer[kernel_addr +  4] * (input_buffer[(input_addr + 4)] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  5] * (input_buffer[(input_addr + 5)] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  6] * (input_buffer[(input_addr + 6)] + input_offset) + 
-          kernel_weights_buffer[kernel_addr +  7] * (input_buffer[(input_addr + 7)] + input_offset);
-
-        kernel_addr = kernel_addr + SUM_AT_ONCE;
-
+      if (update_address) begin
+        kernel_addr <= kernel_addr + SUM_AT_ONCE;
         if ((input_addr + SUM_AT_ONCE) >= cur_buffer_size) begin
-          input_addr = input_addr + SUM_AT_ONCE - cur_buffer_size;
+          input_addr <= input_addr + SUM_AT_ONCE - cur_buffer_size;
         end else begin
-          input_addr = input_addr + SUM_AT_ONCE;
+          input_addr <= input_addr + SUM_AT_ONCE;
         end
-
+        update_address <= 0;
+      end else begin
+        if (kernel_addr >= cur_buffer_size) begin
+          finished_work <= 1;
+        end else begin
+          acc <= acc + 
+            kernel_weights_buffer[kernel_addr     ] * (input_buffer[(input_addr    )] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  1] * (input_buffer[(input_addr + 1)] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  2] * (input_buffer[(input_addr + 2)] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  3] * (input_buffer[(input_addr + 3)] + input_offset) +
+            kernel_weights_buffer[kernel_addr +  4] * (input_buffer[(input_addr + 4)] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  5] * (input_buffer[(input_addr + 5)] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  6] * (input_buffer[(input_addr + 6)] + input_offset) + 
+            kernel_weights_buffer[kernel_addr +  7] * (input_buffer[(input_addr + 7)] + input_offset);
+        end
+        update_address <= 1;
       end
-
 
     end
 
@@ -220,7 +97,8 @@ module conv1d #(
       end
 
       4: begin
-        input_output_width <= value;
+        // input_output_width <= value;
+        ret <= 0;
       end
       5: begin
         input_depth <= value;
@@ -229,8 +107,9 @@ module conv1d #(
       6: begin  // Start computation
         acc <= 0;
         finished_work <= 0;
-        kernel_addr = 0;
-        input_addr  = start_filter_x * input_depth;
+        update_address <= 0;
+        kernel_addr <= 0;
+        input_addr  <= start_filter_x * input_depth;
       end
 
       7: begin  // get acumulator
@@ -242,6 +121,12 @@ module conv1d #(
       9: begin  // Check if computation is done
         ret <= finished_work;
       end
+      // 10: begin
+      //   ret <= input_buffer[address];
+      // end
+      // 11: begin
+      //   ret <= kernel_weights_buffer[address];
+      // end
       default: begin
         // $display("!!! DEFAULT case ");
         ret <= 0;
@@ -268,6 +153,7 @@ module Cfu (
 
   wire [6:0] funct7;
   assign funct7 = cmd_payload_function_id[9:3];
+  // reg wait_reg = 0;
   wire [31:0] ret;
 
   conv1d CONV_1D (
@@ -289,6 +175,14 @@ module Cfu (
       // Waiting to hand off response to CPU.
       rsp_valid <= ~rsp_ready;
     end else if (cmd_valid) begin
+      // if (wait_reg) begin
+      //   wait_reg <= 0;
+      //   rsp_valid <= 0;
+      // end else begin
+      //   wait_reg <= 1;
+      //   rsp_valid <= output_buffer_valid;
+      // end
+
       rsp_valid <= output_buffer_valid;
     end
   end
