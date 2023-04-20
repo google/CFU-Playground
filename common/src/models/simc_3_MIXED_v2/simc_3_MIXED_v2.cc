94cfe27a2189a0996fc0fc3d8ac08fa083e9cd99
Pavlo Hilei <pavlikhiley@gmail.com>
2023-04-20 21:36:11 +0200

    UPD

--- common/src/models/simc_3_MIXED_v2/simc_3_MIXED_v2.cc
+++ common/src/models/simc_3_MIXED_v2/simc_3_MIXED_v2.cc
@@ -81,7 +81,7 @@ static int8_t* simc_3_MIXED_v2_classify() {
 
 #define CFU_INITIALIZE 0
 #define CFU_WRITE_INPUT_BUFFER 1
-#define CFU_WRITE_KERNEL_BUFFER 2
+#define CFU_WRITE_FILTER_BUFFER 2
 #define CFU_WRITE_INPUT_OFFSET 3
 #define CFU_WRITE_INPUT_OUTPUT_WIDTH 4
 #define CFU_WRITE_INPUT_DEPTH 5
@@ -89,6 +89,10 @@ static int8_t* simc_3_MIXED_v2_classify() {
 #define CFU_READ_ACCUMULATOR 7
 #define CFU_WRITE_START_FILTER_X 8
 #define CFU_FINISHED 9
+
+// Temporaty
+#define CFU_READ_INPUT_BUFFER 10
+#define CFU_READ_FILTER_BUFFER 11
 
 #endif  // CFU_CONV1d_V8_PARAMS
 
@@ -128,50 +132,92 @@ void printBits(size_t const size, void const* const ptr) {
 }
 
 [[maybe_unused]] static void test_conv1d_cfu() {
+  const int32_t kernel_length = 8;
   printf("\n==================== Start CFU test (check output) ====================\n");
+  cfu_op0(CFU_INITIALIZE, 0, 0);
+
+  // Write values to the input
+  cfu_op0(CFU_WRITE_INPUT_BUFFER, 0, 123);
+  cfu_op0(CFU_WRITE_INPUT_BUFFER, 1, 255);
+
+  // Read values from the input
+  int8_t v0 = cfu_op0(CFU_READ_INPUT_BUFFER, 0, 0);
+  int8_t v1 = cfu_op0(CFU_READ_INPUT_BUFFER, 1, 0);
+
+  printf("Write/Read input: v0 = %d (expected 123); v1 = %d (expected -1)\n", v0, v1);
+
+  // Write values to the kernel
+  cfu_op0(CFU_WRITE_FILTER_BUFFER, 0, 13);
+  cfu_op0(CFU_WRITE_FILTER_BUFFER, 1, -3);
+
+  // Read values from the kernel
+  v0 = cfu_op0(CFU_READ_FILTER_BUFFER, 0, 0);
+  v1 = cfu_op0(CFU_READ_FILTER_BUFFER, 1, 0);
+
+  printf("Write/Read filter: v0 = %d; (expected 13) v1 = %d (expexted -3) \n", v0, v1);
+
+  // Write input depth for test convolution
+  int32_t input_depth = 1;
+  cfu_op0(CFU_WRITE_INPUT_DEPTH, 0, input_depth);
+
+  // Write input buffer values (8 * input_depth = 8)
+  for (size_t i = 0; i < static_cast<size_t>(kernel_length * input_depth); ++i) {
+    cfu_op0(CFU_WRITE_INPUT_BUFFER, i, i);
+    cfu_op0(CFU_WRITE_FILTER_BUFFER, i, i);
+  }
+
+  // Start computation
+  cfu_op0(CFU_START_COMPUTATION, 0, 0);
+  // Wait for result
+  while (!cfu_op0(CFU_FINISHED, 0, 0)) {
+  }
+  // Get result
+  int32_t acc = cfu_op0(CFU_READ_ACCUMULATOR, 0, 0);
+  // 0**2 + 1**2 + ... 7 **2 = 140
+  printf("Computation: acc = %ld (expected 140)\n", acc);
 
   printf("\n==================== End CFU test ====================\n");
 }
 
 static void do_tests() {
-//  test_conv1d_cfu();
+  test_conv1d_cfu();
 
-  int8_t epsilon = 20;
-  bool failed    = false;
+  // int8_t epsilon = 20;
+  // bool failed    = false;
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_16QAM, pred_simc_3_MIXED_v2_16QAM,
-           epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_16QAM, pred_simc_3_MIXED_v2_16QAM,
+  //          epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_64QAM, pred_simc_3_MIXED_v2_64QAM,
-           epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_64QAM, pred_simc_3_MIXED_v2_64QAM,
+  //          epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_8PSK, pred_simc_3_MIXED_v2_8PSK, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_8PSK, pred_simc_3_MIXED_v2_8PSK, epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_B_FM, pred_simc_3_MIXED_v2_B_FM, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_B_FM, pred_simc_3_MIXED_v2_B_FM, epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_BPSK, pred_simc_3_MIXED_v2_BPSK, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_BPSK, pred_simc_3_MIXED_v2_BPSK, epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_CPFSK, pred_simc_3_MIXED_v2_CPFSK,
-           epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_CPFSK, pred_simc_3_MIXED_v2_CPFSK,
+  //          epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_DSB_AM, pred_simc_3_MIXED_v2_DSB_AM,
-           epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_DSB_AM, pred_simc_3_MIXED_v2_DSB_AM,
+  //          epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_GFSK, pred_simc_3_MIXED_v2_GFSK, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_GFSK, pred_simc_3_MIXED_v2_GFSK, epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_PAM4, pred_simc_3_MIXED_v2_PAM4, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_PAM4, pred_simc_3_MIXED_v2_PAM4, epsilon);
 
-  failed = failed ||
-           perform_one_test(test_data_simc_3_MIXED_v2_QPSK, pred_simc_3_MIXED_v2_QPSK, epsilon);
+  // failed = failed ||
+  //          perform_one_test(test_data_simc_3_MIXED_v2_QPSK, pred_simc_3_MIXED_v2_QPSK, epsilon);
 
   // failed = failed || perform_one_test(
   //   test_data_simc_3_MIXED_v2_SSB_AM,
@@ -179,11 +225,11 @@ static void do_tests() {
   //   epsilon
   // );
 
-  if (failed) {
-    printf("FAIL simc_3_MIXED_v2 tests failed\n");
-  } else {
-    printf("OK   simc_3_MIXED_v2 tests passed\n");
-  }
+  // if (failed) {
+  //   printf("FAIL simc_3_MIXED_v2 tests failed\n");
+  // } else {
+  //   printf("OK   simc_3_MIXED_v2 tests passed\n");
+  // }
 }
 
 static struct Menu MENU = {
--- proj/simc_3_MIXED_v2_software/cfu.v
+++ proj/simc_3_MIXED_v2_software/cfu.v
@@ -1,6 +1,148 @@
 // `include "cfu_v8_clean.sv"
 
-module conv1d #(
+// module conv1d #(
+//     parameter BYTE_SIZE  = 8,
+//     parameter INT32_SIZE = 32
+// ) (
+//     input                       clk,
+//     input      [           6:0] cmd,
+//     input      [INT32_SIZE-1:0] inp0,
+//     input      [INT32_SIZE-1:0] inp1,
+//     output reg [INT32_SIZE-1:0] ret,
+//     output reg                  output_buffer_valid = 1
+// );
+//   localparam PADDING = 4;  // (8 / 2)
+//   localparam MAX_INPUT_SIZE = 1024;
+//   localparam MAX_INPUT_CHANNELS = 128;
+//   localparam KERNEL_LENGTH = 8;
+
+//   localparam SUM_AT_ONCE = 1;
+//   localparam BUFFERS_SIZE = KERNEL_LENGTH * MAX_INPUT_CHANNELS;
+
+//   wire [INT32_SIZE-1:0] address = inp0;
+//   wire [INT32_SIZE-1:0] value = inp1;
+//   wire [INT32_SIZE-1:0] cur_buffer_size = KERNEL_LENGTH * input_depth;
+
+//   // Buffers
+//   (* RAM_STYLE="BLOCK" *)
+//   reg signed [BYTE_SIZE-1:0] input_buffer[0:BUFFERS_SIZE - 1];
+
+//   (* RAM_STYLE="BLOCK" *)
+//   reg signed [BYTE_SIZE-1:0] filter_buffer[0:BUFFERS_SIZE - 1];
+
+//   // Parameters
+//   reg signed [INT32_SIZE-1:0] input_offset = 32'd0;
+//   // reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
+//   reg signed [INT32_SIZE-1:0] input_depth = 32'd0;
+
+//   // Computation related registers
+//   reg signed [INT32_SIZE-1:0] start_filter_x = 0;
+//   reg finished_work = 1'b1;
+//   reg update_address = 1'b0;
+//   reg [INT32_SIZE-1:0] kernel_addr;
+//   reg [INT32_SIZE-1:0] input_addr;
+//   reg signed [INT32_SIZE-1:0] acc;
+
+//   always @(posedge clk) begin
+//     if (!finished_work) begin
+//       if (update_address) begin
+//         kernel_addr <= kernel_addr + SUM_AT_ONCE;
+//         if ((input_addr + SUM_AT_ONCE) >= cur_buffer_size) begin
+//           input_addr <= input_addr + SUM_AT_ONCE - cur_buffer_size;
+//         end else begin
+//           input_addr <= input_addr + SUM_AT_ONCE;
+//         end
+//         update_address <= 0;
+//       end else begin
+//         if (kernel_addr >= cur_buffer_size) begin
+//           finished_work <= 1;
+//         end else begin
+//           acc <= acc + 
+//             filter_buffer[kernel_addr     ] * (input_buffer[(input_addr    )] + input_offset);
+//             // filter_buffer[kernel_addr +  1] * (input_buffer[(input_addr + 1)] + input_offset);
+//             // filter_buffer[kernel_addr +  2] * (input_buffer[(input_addr + 2)] + input_offset) + 
+//             // filter_buffer[kernel_addr +  3] * (input_buffer[(input_addr + 3)] + input_offset) +
+//             // filter_buffer[kernel_addr +  4] * (input_buffer[(input_addr + 4)] + input_offset) + 
+//             // filter_buffer[kernel_addr +  5] * (input_buffer[(input_addr + 5)] + input_offset) + 
+//             // filter_buffer[kernel_addr +  6] * (input_buffer[(input_addr + 6)] + input_offset) + 
+//             // filter_buffer[kernel_addr +  7] * (input_buffer[(input_addr + 7)] + input_offset);
+//         end
+//         update_address <= 1;
+//       end
+
+//     end
+
+//     case (cmd)
+//       // Initialize
+//       0: begin  // Reset module
+//         // Fill input with zeros
+//         ret <= BUFFERS_SIZE;
+//       end
+
+//       // Write buffers
+//       1: begin  // Write input buffer
+//         // if (address < BUFFERS_SIZE) begin
+//         input_buffer[address] <= value[7:0];
+//         // end
+//       end
+//       2: begin  // Write kernel weights buffer
+//         // if (address < BUFFERS_SIZE) begin
+//         filter_buffer[address] <= value[7:0];
+//         // end
+//       end
+
+//       // Write parameters
+//       3: begin
+//         input_offset <= value;
+//       end
+
+//       4: begin
+//         // input_output_width <= value;
+//         ret <= 0;
+//       end
+//       5: begin
+//         input_depth <= value;
+//       end
+
+//       6: begin  // Start computation
+//         acc <= 0;
+//         finished_work <= 0;
+//         update_address <= 0;
+//         kernel_addr <= 0;
+//         input_addr  <= start_filter_x * input_depth;
+//       end
+
+//       7: begin  // get acumulator
+//         ret <= acc;
+//       end
+//       8: begin  // Write start x in input ring buffer 
+//         start_filter_x <= value;
+//       end
+//       9: begin  // Check if computation is done
+//         ret <= finished_work;
+//       end
+//       10: begin
+//         ret <= input_buffer[address];
+//       end
+//       11: begin
+//         ret <= filter_buffer[address];
+//       end
+//       // 10: begin
+//       //   ret <= input_buffer[address];
+//       // end
+//       // 11: begin
+//       //   ret <= filter_buffer[address];
+//       // end
+//       default: begin
+//         // $display("!!! DEFAULT case ");
+//         ret <= 0;
+//       end
+//     endcase
+//   end
+
+// endmodule
+
+module mem #(
     parameter BYTE_SIZE  = 8,
     parameter INT32_SIZE = 32
 ) (
@@ -16,69 +158,23 @@ module conv1d #(
   localparam MAX_INPUT_CHANNELS = 128;
   localparam KERNEL_LENGTH = 8;
 
-  localparam SUM_AT_ONCE = 8;
+  localparam SUM_AT_ONCE = 2;
   localparam BUFFERS_SIZE = KERNEL_LENGTH * MAX_INPUT_CHANNELS;
 
   wire [INT32_SIZE-1:0] address = inp0;
   wire [INT32_SIZE-1:0] value = inp1;
-  wire [INT32_SIZE-1:0] cur_buffer_size = KERNEL_LENGTH * input_depth;
 
   // Buffers
   (* RAM_STYLE="BLOCK" *)
   reg signed [BYTE_SIZE-1:0] input_buffer[0:BUFFERS_SIZE - 1];
 
   (* RAM_STYLE="BLOCK" *)
-  reg signed [BYTE_SIZE-1:0] kernel_weights_buffer[0:BUFFERS_SIZE - 1];
+  reg signed [BYTE_SIZE-1:0] filter_buffer[0:BUFFERS_SIZE - 1];
 
-  // Parameters
-  reg signed [INT32_SIZE-1:0] input_offset = 32'd0;
-  // reg signed [INT32_SIZE-1:0] input_output_width = 32'd0;
-  reg signed [INT32_SIZE-1:0] input_depth = 32'd0;
-
-  // Computation related registers
-  reg signed [INT32_SIZE-1:0] start_filter_x = 0;
-  reg finished_work = 1'b1;
-  reg update_address = 1'b0;
-  reg [INT32_SIZE-1:0] kernel_addr;
-  reg [INT32_SIZE-1:0] input_addr;
   reg signed [INT32_SIZE-1:0] acc;
 
   always @(posedge clk) begin
-    if (!finished_work) begin
-      if (update_address) begin
-        kernel_addr <= kernel_addr + SUM_AT_ONCE;
-        if ((input_addr + SUM_AT_ONCE) >= cur_buffer_size) begin
-          input_addr <= input_addr + SUM_AT_ONCE - cur_buffer_size;
-        end else begin
-          input_addr <= input_addr + SUM_AT_ONCE;
-        end
-        update_address <= 0;
-      end else begin
-        if (kernel_addr >= cur_buffer_size) begin
-          finished_work <= 1;
-        end else begin
-          acc <= acc + 
-            kernel_weights_buffer[kernel_addr     ] * (input_buffer[(input_addr    )] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  1] * (input_buffer[(input_addr + 1)] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  2] * (input_buffer[(input_addr + 2)] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  3] * (input_buffer[(input_addr + 3)] + input_offset) +
-            kernel_weights_buffer[kernel_addr +  4] * (input_buffer[(input_addr + 4)] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  5] * (input_buffer[(input_addr + 5)] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  6] * (input_buffer[(input_addr + 6)] + input_offset) + 
-            kernel_weights_buffer[kernel_addr +  7] * (input_buffer[(input_addr + 7)] + input_offset);
-        end
-        update_address <= 1;
-      end
-
-    end
-
     case (cmd)
-      // Initialize
-      0: begin  // Reset module
-        // Fill input with zeros
-        ret <= BUFFERS_SIZE;
-      end
-
       // Write buffers
       1: begin  // Write input buffer
         // if (address < BUFFERS_SIZE) begin
@@ -87,46 +183,26 @@ module conv1d #(
       end
       2: begin  // Write kernel weights buffer
         // if (address < BUFFERS_SIZE) begin
-        kernel_weights_buffer[address] <= value[7:0];
+        filter_buffer[address] <= value[7:0];
         // end
       end
-
-      // Write parameters
-      3: begin
-        input_offset <= value;
+      6: begin
+        acc <= input_buffer[2] * filter_buffer[2];
       end
-
-      4: begin
-        // input_output_width <= value;
-        ret <= 0;
-      end
-      5: begin
-        input_depth <= value;
-      end
-
-      6: begin  // Start computation
-        acc <= 0;
-        finished_work <= 0;
-        update_address <= 0;
-        kernel_addr <= 0;
-        input_addr  <= start_filter_x * input_depth;
-      end
-
-      7: begin  // get acumulator
+      7: begin
         ret <= acc;
       end
-      8: begin  // Write start x in input ring buffer 
-        start_filter_x <= value;
+      9: begin
+        ret <= 1;
       end
-      9: begin  // Check if computation is done
-        ret <= finished_work;
+
+
+      10: begin // Read kernel buffer
+        ret <= input_buffer[address];
       end
-      // 10: begin
-      //   ret <= input_buffer[address];
-      // end
-      // 11: begin
-      //   ret <= kernel_weights_buffer[address];
-      // end
+      11: begin // Read kernel buffer
+        ret <= filter_buffer[address];
+      end
       default: begin
         // $display("!!! DEFAULT case ");
         ret <= 0;
@@ -156,7 +232,8 @@ module Cfu (
   // reg wait_reg = 0;
   wire [31:0] ret;
 
-  conv1d CONV_1D (
+  // conv1d CONV_1D (
+  mem MEM (
       .clk(clk),
       .cmd(funct7),
       .inp0(cmd_payload_inputs_0),
